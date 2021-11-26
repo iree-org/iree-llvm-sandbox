48104207f271c2cd243e77dbe535d7c58cfec7d6
Alexander Belyaev <32522095+pifon2a@users.noreply.github.com>
2021-11-26 11:05:33 +0100

    Add tests/benchmarks for 1d reduction. (#42)

---
+++ python/reduction/reduction_1d_bench.py
@@ -0,0 +1,78 @@
+# RUN: %PYTHON %s 2>&1 | FileCheck %s
+
+# This file contains small benchmarks with reasonably-sized problem/tiling sizes
+# and codegen options.
+
+from ..core.experts import *
+from ..core.harness import *
+from ..core.transforms import *
+
+from .definitions import *
+
+fun_name = 'reduction_1d_on_tensors'
+op_name = 'linalg.generic'
+
+################################################################################
+### Compilation strategies.
+################################################################################
+
+
+def all_experts(problem_sizes: List[int]):
+  return [
+      SingleTilingExpert(
+          fun_name=fun_name,
+          op_name=op_name,
+          sizes=[32],
+          interchange=[],
+          peel=[],
+          pad=False,
+          pack_paddings=[],
+          hoist_paddings=[],
+          # kwargs passed down to LowerVectors.
+          # TODO: better composition of experts.
+          multi_reduction_lowering='innerreduction',
+          print_ir_after_all=False),
+  ]
+
+################################################################################
+### Problem instantiations.
+################################################################################
+
+keys = ['M']
+
+
+def make_size_list(sizes: Sequence):
+  return {k: v for k, v in zip(keys, sizes)}
+
+# CHECK-NOT: FAILURE
+def main():
+  n_iters = 100
+  problem_size_list = [
+      [128],
+      [104],
+      [256],
+      [1000],
+      [8000],
+  ]
+
+  def numpy_kernel(args, sizes, types):
+    A, B = args
+    B.fill(0.)
+    np.sum(A, out=B)
+
+  def pytorch_kernel(args, sizes, types):
+    A, B = args
+    B.fill_(0.)
+    torch.sum(A, out=B)
+
+  for problem_sizes in problem_size_list:
+    test_harness(
+        lambda s, t: Reduction1DProblem(), [[np.float32] * 2],
+        map(make_size_list, [problem_sizes]),
+        all_experts(problem_sizes),
+        n_iters=n_iters,
+        function_name=fun_name)
+
+
+if __name__ == '__main__':
+  main()
---
+++ python/reduction/reduction_1d_test.py
@@ -0,0 +1,45 @@
+# RUN: %PYTHON %s 2>&1 | FileCheck %s
+
+# This file contains simple test cases that combine various codegen options.
+
+from ..core.experts import *
+from ..core.harness import *
+from ..core.problem_definition import *
+from ..core.transforms import *
+
+from .definitions import *
+
+################################################################################
+### Compilation strategies.
+################################################################################
+
+# No tiling.
+expert_no_tiling = LoweringOnlyExpert([], print_ir_after_all=False)
+
+all_experts = [expert_no_tiling]
+
+################################################################################
+### Problem instantiations.
+################################################################################
+
+keys = ['M']
+
+
+def make_size_list(sizes: Sequence):
+  return {k: v for k, v in zip(keys, sizes)}
+
+# CHECK-NOT: FAILURE
+def main():
+  n_iters = 1
+  problem_size_list = [[48], [49]]
+
+  test_harness(
+      lambda s, t: Reduction1DProblem(), [[np.float32] * 2],
+      map(make_size_list, problem_size_list),
+      all_experts,
+      n_iters=n_iters,
+      function_name='reduction_1d_on_tensors')
+
+
+if __name__ == '__main__':
+  main()
