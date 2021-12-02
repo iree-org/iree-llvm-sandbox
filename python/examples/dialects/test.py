d476a97f858b91a3f290006f942f5681e2ef58c6
Nicolas Vasilache <nicolas.vasilache@gmail.com>
2021-12-02 19:56:12 +0000

    Address post-commit review comments for #75

--- python/examples/dialects/test.py
+++ python/examples/dialects/test.py
@@ -1,7 +1,4 @@
-import os
-
 import mlir.dialects.linalg_ext as linalg_ext
 
-assert linalg_ext.YieldOp is not None, \
-    "Could not load linalg_ext.YieldOp"
+assert hasattr(linalg_ext, 'YieldOp'), 'Could not load linalg_ext.YieldOp'
 
