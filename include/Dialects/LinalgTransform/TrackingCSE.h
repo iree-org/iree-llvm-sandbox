9aac461255c42c4776da4f6c69896672fd546a5b
Alex Zinenko <zinenko@google.com>
2021-12-21 10:52:02 +0000

    Add file-level comments to TrackingCSE

--- include/Dialects/LinalgTransform/TrackingCSE.h
+++ include/Dialects/LinalgTransform/TrackingCSE.h
@@ -1,3 +1,11 @@
+//===-- TrackingCSE.h - Special common subexpr elimination ------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef MLIR_DIALECT_LINALG_TRANSFORMS_TRACKINGCSE_H
 #define MLIR_DIALECT_LINALG_TRANSFORMS_TRACKINGCSE_H
 
--- lib/Dialects/LinalgTransform/Transforms/TrackingCSE.cpp
+++ lib/Dialects/LinalgTransform/Transforms/TrackingCSE.cpp
@@ -1,3 +1,11 @@
+//===-- TrackingCSE.cpp - Common subexpr elimination keeping track of ops -===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "Dialects/LinalgTransform/TrackingCSE.h"
 #include "mlir/IR/Dominance.h"
 #include "mlir/IR/OpDefinition.h"
