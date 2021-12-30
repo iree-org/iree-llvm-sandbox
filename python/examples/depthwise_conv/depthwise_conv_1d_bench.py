c5e48f430d06d39b662ec1f0ee197d7b7058cdbb
Nicolas Vasilache <nicolas.vasilache@gmail.com>
2021-12-30 10:47:42 +0000

    Add depthwise_conv_2d examples

--- python/examples/depthwise_conv/depthwise_conv_1d_bench.py
+++ python/examples/depthwise_conv/depthwise_conv_1d_bench.py
@@ -12,50 +12,54 @@ fun_name = 'depthwise_conv_1d_nwc_wc'
 op_name = 'linalg.depthwise_conv_1d_nwc_wc'
 
 ################################################################################
-### Compilation strategies.
+# Compilation strategies.
 ################################################################################
 
 all_experts = [
-    # LoweringOnlyExpert().print_ir(after_all=False),
-    SingleTilingExpert(
-        fun_name=fun_name,
-        op_name=op_name,
-        #           N  W   C  KW
-        tile_sizes=[1, 4, 16, 3]).print_ir(after_all=False)
-]
+    e.print_ir(after_all=False, at_begin=False, llvm=False) for e in [
+        SingleTilingExpert(
+            fun_name=fun_name,
+            op_name=op_name,
+            #           N  W   C  KW
+            # tile_sizes=[1, 4, 16, 3]
+            tile_sizes=[4, 4, 16, 3])
+    ]]
 
 ################################################################################
-### Problem instantiation
+# Problem instantiation
 ################################################################################
 
 keys = ['N', 'W', 'C', 'KW', 'strides', 'dilations']
 
 
 def make_size_list(keys: Sequence[str], sizes: Sequence):
-  return {k: v for k, v in zip(keys, sizes)}
+    return {k: v for k, v in zip(keys, sizes)}
+
 
 # CHECK-NOT: FAILURE
 def main():
-  n_iters = 1000
-  #   N   W   C  KW   st  dil
-  problem_size_list = [\
-     [8, 16, 32,  3, [1],  [1]], \
-     [8, 16, 32,  3, [1],  [2]], \
-     [8, 16, 32,  3, [2],  [1]], \
-     [8, 16, 32,  3, [2],  [2]],  \
-     [8, 16, 32,  3, [2],  [3]],  \
-     [8, 16, 32,  3, [3],  [2]]  \
-  ]
+    n_iters = 1000
+    #   N   W   C  KW   st  dil
+    problem_size_list = [
+        [8, 16, 32,  3, [1],  [1]],
+        [8, 16, 32,  3, [1],  [2]],
+        [8, 16, 32,  3, [2],  [1]],
+        [8, 16, 32,  3, [2],  [2]],
+        [8, 16, 32,  3, [2],  [3]],
+        [8, 16, 32,  3, [3],  [2]]
+    ]
 
-  test_harness(
-      lambda sizes, t: DepthwiseConvolutionProblem(
-          'NWC', 'WC', strides=sizes['strides'], dilations=sizes['dilations']),
-      [[np.float32] * 3],
-      map(lambda sizes: make_size_list(keys, sizes), problem_size_list),
-      all_experts,
-      n_iters=n_iters,
-      function_name=fun_name)
+    test_harness(
+        lambda sizes, t: DepthwiseConvolutionProblem(
+            'NWC', 'WC', strides=sizes['strides'], dilations=sizes['dilations']),
+        [[np.float32] * 3],
+        map(lambda sizes: make_size_list(keys, sizes), problem_size_list),
+        all_experts,
+        n_iters=n_iters,
+        function_name=fun_name,
+        dump_ir_to_file='/tmp/abcd.mlir',
+        dump_obj_to_file='/tmp/abcd.o',)
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- python/examples/depthwise_conv/ops.py
+++ python/examples/depthwise_conv/ops.py
@@ -158,3 +158,148 @@ def depthwise_conv_1d_wcn_wc(
   domain(D.ow, D.c, D.n, D.kw)
   O[D.ow, D.c, D.n] += (
       cast(U, I[D.ow * S.SW + D.kw * S.DW, D.c, D.n]) * cast(U, K[D.kw, D.c]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_nchw_chw(
+    I=TensorDef(TV.T1, S.N, S.C, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW),
+    K=TensorDef(TV.T2, S.C, S.KH, S.KW),
+    O=TensorDef(U, S.N, S.C, S.OH, S.OW, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.n, D.c, D.oh, D.ow, D.kh, D.kw)
+  O[D.n, D.c, D.oh, D.ow] += (cast(U, I[D.n, D.c, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW]) * cast(U, K[D.c, D.kh, D.kw]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_nchw_hwc(
+    I=TensorDef(TV.T1, S.N, S.C, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW),
+    K=TensorDef(TV.T2, S.KH, S.KW, S.C),
+    O=TensorDef(U, S.N, S.C, S.OH, S.OW, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.n, D.c, D.oh, D.ow, D.kh, D.kw)
+  O[D.n, D.c, D.oh, D.ow] += (cast(U, I[D.n, D.c, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW]) * cast(U, K[D.kh, D.kw, D.c]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_nhwc_chw(
+    I=TensorDef(TV.T1, S.N, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.C),
+    K=TensorDef(TV.T2, S.C, S.KH, S.KW),
+    O=TensorDef(U, S.N, S.OH, S.OW, S.C, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.n, D.oh, D.ow, D.c, D.kh, D.kw)
+  O[D.n, D.oh, D.ow, D.c] += (cast(U, I[D.n, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.c]) * cast(U, K[D.c, D.kh, D.kw]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_nhwc_hwc(
+    I=TensorDef(TV.T1, S.N, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.C),
+    K=TensorDef(TV.T2, S.KH, S.KW, S.C),
+    O=TensorDef(U, S.N, S.OH, S.OW, S.C, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.n, D.oh, D.ow, D.c, D.kh, D.kw)
+  O[D.n, D.oh, D.ow, D.c] += (cast(U, I[D.n, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.c]) * cast(U, K[D.kh, D.kw, D.c]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_cnhw_chw(
+    I=TensorDef(TV.T1, S.C, S.N, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW),
+    K=TensorDef(TV.T2, S.C, S.KH, S.KW),
+    O=TensorDef(U, S.C, S.N, S.OH, S.OW, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.c, D.n, D.oh, D.ow, D.kh, D.kw)
+  O[D.c, D.n, D.oh, D.ow] += (cast(U, I[D.c, D.n, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW]) * cast(U, K[D.c, D.kh, D.kw]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_cnhw_hwc(
+    I=TensorDef(TV.T1, S.C, S.N, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW),
+    K=TensorDef(TV.T2, S.KH, S.KW, S.C),
+    O=TensorDef(U, S.C, S.N, S.OH, S.OW, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.c, D.n, D.oh, D.ow, D.kh, D.kw)
+  O[D.c, D.n, D.oh, D.ow] += (cast(U, I[D.c, D.n, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW]) * cast(U, K[D.kh, D.kw, D.c]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_chwn_chw(
+    I=TensorDef(TV.T1, S.C, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.N),
+    K=TensorDef(TV.T2, S.C, S.KH, S.KW),
+    O=TensorDef(U, S.C, S.OH, S.OW, S.N, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.c, D.oh, D.ow, D.n, D.kh, D.kw)
+  O[D.c, D.oh, D.ow, D.n] += (cast(U, I[D.c, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.n]) * cast(U, K[D.c, D.kh, D.kw]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_chwn_hwc(
+    I=TensorDef(TV.T1, S.C, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.N),
+    K=TensorDef(TV.T2, S.KH, S.KW, S.C),
+    O=TensorDef(U, S.C, S.OH, S.OW, S.N, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.c, D.oh, D.ow, D.n, D.kh, D.kw)
+  O[D.c, D.oh, D.ow, D.n] += (cast(U, I[D.c, D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.n]) * cast(U, K[D.kh, D.kw, D.c]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_hwnc_chw(
+    I=TensorDef(TV.T1, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.N, S.C),
+    K=TensorDef(TV.T2, S.C, S.KH, S.KW),
+    O=TensorDef(U, S.OH, S.OW, S.N, S.C, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.oh, D.ow, D.n, D.c, D.kh, D.kw)
+  O[D.oh, D.ow, D.n, D.c] += (cast(U, I[D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.n, D.c]) * cast(U, K[D.c, D.kh, D.kw]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_hwnc_hwc(
+    I=TensorDef(TV.T1, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.N, S.C),
+    K=TensorDef(TV.T2, S.KH, S.KW, S.C),
+    O=TensorDef(U, S.OH, S.OW, S.N, S.C, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.oh, D.ow, D.n, D.c, D.kh, D.kw)
+  O[D.oh, D.ow, D.n, D.c] += (cast(U, I[D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.n, D.c]) * cast(U, K[D.kh, D.kw, D.c]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_hwcn_chw(
+    I=TensorDef(TV.T1, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.C, S.N),
+    K=TensorDef(TV.T2, S.C, S.KH, S.KW),
+    O=TensorDef(U, S.OH, S.OW, S.C, S.N, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.oh, D.ow, D.c, D.n, D.kh, D.kw)
+  O[D.oh, D.ow, D.c, D.n] += (cast(U, I[D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.c, D.n]) * cast(U, K[D.c, D.kh, D.kw]))
+
+
+@linalg_structured_op
+def depthwise_conv_2d_hwcn_hwc(
+    I=TensorDef(TV.T1, S.OH * S.SH + S.KH * S.DH, S.OW * S.SW + S.KW * S.DW, S.C, S.N),
+    K=TensorDef(TV.T2, S.KH, S.KW, S.C),
+    O=TensorDef(U, S.OH, S.OW, S.C, S.N, output=True),
+    strides=AttributeDef(S.SH, S.SW),
+    dilations=AttributeDef(S.DH, S.DW)):
+  implements(ConvolutionOpInterface)
+  domain(D.oh, D.ow, D.c, D.n, D.kh, D.kw)
+  O[D.oh, D.ow, D.c, D.n] += (cast(U, I[D.oh * S.SH + D.kh * S.DH, D.ow * S.SW + D.kw * S.DW, D.c, D.n]) * cast(U, K[D.kh, D.kw, D.c]))
+
