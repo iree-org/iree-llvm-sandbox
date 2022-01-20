58196b4704078f56533f8b5810a6f169f837ce05
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2022-01-20 12:01:30 +0100

    Workaround memref.copy perf problems (#200)

--- python/examples/copy/copy_1d_bench.py
+++ python/examples/copy/copy_1d_bench.py
@@ -12,33 +12,27 @@ from ..contraction.definitions import EinsumProblem
 
 from typing import List
 
-base_fun_name = 'copy_1d_on_tensors'
-op_name = 'linalg.generic'
-
 ################################################################################
 ### Compilation strategies.
 ################################################################################
 
-
 # Before bufferization, the IR only has a tensor.extract_slice /
 #   tensor.insert_slice pair.
-# Bufferization then properly introduces linalg.copy ops.
-# We want to make more these `linalg.copy` more efficient.
+# Bufferization then properly introduces copy ops on buffers.
+# We want to make more these copy more efficient.
 # In the case of a single copy benchmark it is the one true thing to optimize.
-def all_experts(fun_name: str):
-  return [
+all_experts = [
     # Note: `\` char at the end of next line prevents formatter reflows, keep it.
     e.print_ir(after_all=False, at_begin=False, llvm=False) for e in [         \
-      Tile(fun_name=fun_name,
-            op_name=op_name,
-            tile_sizes=[16])
+      Tile(fun_name='copy_1d',
+            op_name='linalg.generic',
+            tile_sizes=[16]) # Fixed tile size
       .then(Bufferize())
-      .then(Vectorize(fun_name=fun_name, op_name='linalg.copy'))
+      .then(Vectorize(fun_name='copy_1d', op_name='linalg.generic'))
       .then(LowerVectors())
       .then(LowerToLLVM())
     ]
-  ]
-
+]
 
 ################################################################################
 ### Problem instantiations.
@@ -46,26 +40,52 @@ def all_experts(fun_name: str):
 
 keys = ['n']
 
-copy_1D_perf_search_list = [
-    [200 * 16],  # sweet spot for prefetchers
-    [112 * 112 * 32],  # approx. depthwise_conv_2d size
-]
-
 
 # CHECK-NOT: FAILURE
 def main():
-  n_iters = 10000
-  for problem_sizes in copy_1D_perf_search_list:
-    fun_name = base_fun_name + '_offset_0' + \
-          '_sizes' + ''.join('_' + str(sz) for sz in problem_sizes)
-    test_harness(lambda s, t: EinsumProblem('n->n', 0),
-                 [[np.float32] * 2],
-                 test_sizes(keys, [problem_sizes]),
-                 all_experts(fun_name),
-                 n_iters=n_iters,
-                 function_name=fun_name,
-                 dump_ir_to_file='/tmp/abc.mlir',
-                 dump_obj_to_file='/tmp/abc.o')
+  # Specify default configuration and parse command line.
+  args = test_argparser(
+      "copy 1d benchmark",
+      default_n_iters=100,
+      default_problem_sizes_list=[
+          [100 * 32],
+          [100 * 272],
+          [200 * 272],
+          [300 * 272],
+          [400 * 272],
+          [500 * 272],
+          [600 * 272],
+          [700 * 272],
+          [800 * 272],
+          [900 * 272],
+          [1000 * 272],
+          [10000 * 272],
+          [20000 * 272],
+          [30000 * 272],
+          [300000 * 272],
+      ],
+      default_expert_list=["Default"],
+      default_dynamic_at_compile_time_list=[
+          [],  # case 1: static at compile time
+      ],
+      default_spec_list=['n->n'],
+  )
+
+  for dynamic_at_compile_time in args.dynamic_at_compile_time_list:
+    for spec in args.spec_list:
+      test_harness(
+          lambda s, t: EinsumProblem('n->n', 0),
+          [[np.float32] * 2],
+          test_sizes(keys, args.problem_sizes_list),
+          test_experts(all_experts, ["Default"], args.expert_list),
+          n_iters=args.n_iters,
+          dynamic_at_compile_time_sizes=set(
+              dynamic_at_compile_time).intersection(keys),
+          function_name='copy_1d',
+          dump_ir_to_file='/tmp/abc.mlir',
+          dump_obj_to_file='/tmp/abc.o',
+          dump_data_to_file=args.dump_data,
+      )
 
 
 if __name__ == '__main__':
