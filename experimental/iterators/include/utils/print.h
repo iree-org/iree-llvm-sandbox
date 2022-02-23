a8a54b3a6f0bbd4e8cb6aafe818fdbe0ecfcbb05
Ingo Müller <ingomueller@google.com>
2022-02-23 10:38:06 +0000

    Iterators: Add tests for PrintTuple. Add ostream param. Fix uncovered bug.

--- experimental/iterators/include/utils/print.h
+++ experimental/iterators/include/utils/print.h
@@ -6,23 +6,28 @@
 
 namespace impl {
 template <typename TupleType, std::size_t... kIndices>
-void PrintTupleTail(const TupleType &tuple,
+void PrintTupleTail(std::ostream &ostream, const TupleType &tuple,
                     const std::index_sequence<kIndices...> & /*unused*/) {
-  ((std::cout << ", " << std::get<kIndices>(tuple)), ...);
+  ((ostream << ", " << std::get<kIndices + 1>(tuple)), ...);
 }
 }  // namespace impl
 
 template <typename... Types>
-void PrintTuple(const std::tuple<Types...> &tuple) {
+void PrintTuple(std::ostream &ostream, const std::tuple<Types...> &tuple) {
   // Print first element
-  std::cout << "(" << std::get<0>(tuple);
+  ostream << "(" << std::get<0>(tuple);
 
   // Print remaining elements
   using TupleType = std::remove_reference_t<decltype(tuple)>;
   using IndexSequence =
       std::make_index_sequence<std::tuple_size_v<TupleType> - 1>;
-  impl::PrintTupleTail(tuple, IndexSequence{});
-  std::cout << ")";
+  impl::PrintTupleTail(ostream, tuple, IndexSequence{});
+  ostream << ")";
+}
+
+template <typename... Types>
+void PrintTuple(const std::tuple<Types...> &tuple) {
+    PrintTuple(std::cout, tuple);
 }
 
 #endif  // UTILS_PRINT_H_
