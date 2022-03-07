3b9aebf088f6a42365aeccc32f8383a9b72ae1a9
Ingo Müller <ingomueller@google.com>
2022-03-07 20:10:34 +0000

    Add simple lit.cfg for FileCheck tests and make first test case complete

---
+++ experimental/iterators/test/lit.cfg.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+import lit.formats
+import lit.util
+
+import lit.llvm
+
+# Configuration file for the 'lit' test runner.
+lit.llvm.initialize(lit_config, config)
+
+# name: The name of this test suite.
+config.name = "Iterators MLIR"
+
+config.test_format = lit.formats.ShTest(execute_external=True)
+
+# suffixes: A list of file extensions to treat as test files.
+config.suffixes = [".mlir"]
+
+# test_source_root: The root path where tests are located.
+config.test_source_root = os.path.dirname(__file__)
+
+# test_exec_root: The path where tests shall execute.
+build_dir = os.environ["IREE_LLVM_SANDBOX_BUILD_DIR"]
+config.test_exec_root = os.path.join(build_dir, 'test')
+lib_dir = os.path.join(build_dir, 'lib')
+
+#config.use_default_substitutions()
+config.excludes = [
+    "lit.cfg.py",
+    "lit.site.cfg.py",
+]
+
+config.substitutions.extend([
+    ("%PYTHON", sys.executable),
+    ("%mlir_runner_utils_dir", lib_dir),
+    ("%shlibext", ".so"),
+])
+
+# Add the build/bin directory to the path.
+sys.path.append(os.path.join(build_dir, "bin"))
+config.environment["PYTHONPATH"] = ":".join(sys.path)
+config.environment["PATH"] = ":".join(sys.path)
+project_root = os.path.dirname(os.path.dirname(__file__))
--- experimental/iterators/test/simple.mlir
+++ experimental/iterators/test/simple.mlir
@@ -1,9 +1,7 @@
-// TODO: this currently does not propertly connect to an actual execution
-
-// R-UN: mlir-opt %s -convert-std-to-llvm -reconcile-unrealized-casts | \
-// R-UN: mlir-cpu-runner -e main -entry-point-result=void \
-// R-UN:   -shared-libs=$(pwd)/lib/Utils/libruntime_utils.so \
-// R-UN: | FileCheck %s
+// RUN: mlir-opt %s -convert-std-to-llvm -reconcile-unrealized-casts | \
+// RUN: mlir-cpu-runner -e main -entry-point-result=void \
+// RUN:   -shared-libs=%mlir_runner_utils_dir/libruntime_utils%shlibext \
+// RUN: | FileCheck %s
 
 func private @oneWay() -> (i64)
 func private @otherWay(i64)
