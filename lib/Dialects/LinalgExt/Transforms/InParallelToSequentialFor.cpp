c82eaa65e73033d6162062cbc42a9a8d1a1c16f8
Nicolas Vasilache <nicolas.vasilache@gmail.com>
2022-03-10 07:46:08 -0500

    Port rewriting of linalg_ext::InParallelOp to scf::ForOp to the transform dialect.
    In the process, retire the InParallelToSequentialFor pass.

--- lib/Dialects/LinalgExt/Transforms/InParallelToSequentialFor.cpp
+++ lib/Dialects/LinalgExt/Transforms/InParallelToSequentialFor.cpp
@@ -12,6 +12,7 @@
 #include "Dialects/LinalgExt/LinalgExtOps.h"
 #include "Dialects/LinalgExt/PassDetail.h"
 #include "Dialects/LinalgExt/Passes.h"
+#include "Dialects/LinalgExt/Transforms/Transforms.h"
 #include "Transforms/Utils.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
@@ -34,103 +35,81 @@ SmallVector<Value> getValuesToYield(PerformConcurrentlyOp op) {
       op.yieldingOps(), [](ParallelInsertSliceOp op) { return op.dest(); }));
 }
 
-struct InParallelOpToSCFRewriter
-    : public OpRewritePattern<linalg_ext::InParallelOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(linalg_ext::InParallelOp inParallelOp,
-                                PatternRewriter &rewriter) const override {
-    // Construct the loop bounds based on the canonical arithmetic progression.
-    Location loc = inParallelOp.getLoc();
-    Value zero = rewriter.create<arith::ConstantIndexOp>(loc, 0);
-    Value one = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-    Value numThreads = inParallelOp.num_threads();
-
-    // Construct the op without a body builder: we need to clone the ops in the
-    // body explicitly after having access to the new bbArgs.
-    // As a consequence, `ensureTerminator` is not called and the `forOp` body
-    // has no terminator.
-    PerformConcurrentlyOp performConcurrentlyOp = inParallelOp.getTerminator();
-    SmallVector<Value> valuesToYield = getValuesToYield(performConcurrentlyOp);
-    scf::ForOp forOp =
-        rewriter.create<scf::ForOp>(loc, zero, numThreads, one, valuesToYield);
-
-    // Move the body while replacing the threadId by the forOp iv.
-    SmallVector<Value> bbArgsTranslated{forOp.getInductionVar()};
-    Block *body = forOp.getBody();
-    bool hasTerminator =
-        !body->empty() && body->back().hasTrait<OpTrait::IsTerminator>();
-    if (hasTerminator) {
-      rewriter.mergeBlockBefore(&inParallelOp.region().front(),
-                                body->getTerminator(), bbArgsTranslated);
-    } else {
-      rewriter.mergeBlocks(&inParallelOp.region().front(), body,
-                           bbArgsTranslated);
-    }
-
-    rewriter.setInsertionPointToStart(body);
-    BlockAndValueMapping bvm;
-    bvm.map(valuesToYield, forOp.getRegionIterArgs());
-
-    // Create sequential insertSlice ops.
-    SmallVector<Value> toYield;
-    rewriter.setInsertionPoint(performConcurrentlyOp);
-    for (ParallelInsertSliceOp op : performConcurrentlyOp.yieldingOps()) {
-      toYield.push_back(rewriter.createOrFold<tensor::InsertSliceOp>(
-          loc, op.source(), bvm.lookup(op.dest()), op.getMixedOffsets(),
-          op.getMixedSizes(), op.getMixedStrides()));
-    }
-
-    // performConcurrentlyOp.yieldedValues come from above, not from bbArgs.
-    // There is no rewriter method to make mergeBlocks update non-bbArgs.
-    // Need to manually clone + bvm all uses that are now nested under forOp.
-    // Warning: this replacement is currently optimistic and may change the
-    // semantics as explained in the pass description in Passes.td.
-    SmallVector<Operation *> opsToReplace;
-    for (Value toReplace : valuesToYield) {
-      for (OpOperand &u : toReplace.getUses()) {
-        Operation *op = u.getOwner();
-        if (!forOp->isProperAncestor(op))
-          continue;
-        opsToReplace.push_back(op);
-      }
-    }
-    for (Operation *op : opsToReplace) {
-      OpBuilder::InsertionGuard g(rewriter);
-      rewriter.setInsertionPoint(op);
-      Operation *cloned = rewriter.clone(*op, bvm);
-      rewriter.replaceOp(op, cloned->getResults());
-    }
-
-    // Insert terminator.
-    if (!hasTerminator) {
-      rewriter.setInsertionPointToEnd(body);
-      rewriter.create<scf::YieldOp>(loc, toYield);
-    }
-
-    // Cleanup and replace.
-    rewriter.eraseOp(performConcurrentlyOp);
-    rewriter.replaceOp(inParallelOp, forOp.getResults());
-
-    return success();
-  }
-};
-
-struct InParallelToSequentialForPass
-    : public InParallelToSequentialForBase<InParallelToSequentialForPass> {
-  void runOnOperation() override;
-};
 } // namespace
 
-void InParallelToSequentialForPass::runOnOperation() {
-  FuncOp funcOp = getOperation();
-  MLIRContext *context = funcOp.getContext();
-  RewritePatternSet patterns(context);
-  patterns.insert<InParallelOpToSCFRewriter>(context);
-  (void)applyPatternsAndFoldGreedily(funcOp, std::move(patterns));
-}
+FailureOr<scf::ForOp> InParallelOpToScfForRewriter::returningMatchAndRewrite(
+    linalg_ext::InParallelOp inParallelOp, PatternRewriter &rewriter) const {
+  // Construct the loop bounds based on the canonical arithmetic progression.
+  Location loc = inParallelOp.getLoc();
+  Value zero = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+  Value one = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+  Value numThreads = inParallelOp.num_threads();
 
-std::unique_ptr<OperationPass<FuncOp>>
-mlir::linalg_ext::createInParallelToSequentialForPass() {
-  return std::make_unique<InParallelToSequentialForPass>();
+  // Construct the op without a body builder: we need to clone the ops in the
+  // body explicitly after having access to the new bbArgs.
+  // As a consequence, `ensureTerminator` is not called and the `forOp` body
+  // has no terminator.
+  PerformConcurrentlyOp performConcurrentlyOp = inParallelOp.getTerminator();
+  SmallVector<Value> valuesToYield = getValuesToYield(performConcurrentlyOp);
+  scf::ForOp forOp =
+      rewriter.create<scf::ForOp>(loc, zero, numThreads, one, valuesToYield);
+
+  // Move the body while replacing the threadId by the forOp iv.
+  SmallVector<Value> bbArgsTranslated{forOp.getInductionVar()};
+  Block *body = forOp.getBody();
+  bool hasTerminator =
+      !body->empty() && body->back().hasTrait<OpTrait::IsTerminator>();
+  if (hasTerminator) {
+    rewriter.mergeBlockBefore(&inParallelOp.region().front(),
+                              body->getTerminator(), bbArgsTranslated);
+  } else {
+    rewriter.mergeBlocks(&inParallelOp.region().front(), body,
+                         bbArgsTranslated);
+  }
+
+  rewriter.setInsertionPointToStart(body);
+  BlockAndValueMapping bvm;
+  bvm.map(valuesToYield, forOp.getRegionIterArgs());
+
+  // Create sequential insertSlice ops.
+  SmallVector<Value> toYield;
+  rewriter.setInsertionPoint(performConcurrentlyOp);
+  for (ParallelInsertSliceOp op : performConcurrentlyOp.yieldingOps()) {
+    toYield.push_back(rewriter.createOrFold<tensor::InsertSliceOp>(
+        loc, op.source(), bvm.lookup(op.dest()), op.getMixedOffsets(),
+        op.getMixedSizes(), op.getMixedStrides()));
+  }
+
+  // performConcurrentlyOp.yieldedValues come from above, not from bbArgs.
+  // There is no rewriter method to make mergeBlocks update non-bbArgs.
+  // Need to manually clone + bvm all uses that are now nested under forOp.
+  // Warning: this replacement is currently optimistic and may change the
+  // semantics as explained in the pass description in Passes.td.
+  SmallVector<Operation *> opsToReplace;
+  for (Value toReplace : valuesToYield) {
+    for (OpOperand &u : toReplace.getUses()) {
+      Operation *op = u.getOwner();
+      if (!forOp->isProperAncestor(op))
+        continue;
+      opsToReplace.push_back(op);
+    }
+  }
+  for (Operation *op : opsToReplace) {
+    OpBuilder::InsertionGuard g(rewriter);
+    rewriter.setInsertionPoint(op);
+    Operation *cloned = rewriter.clone(*op, bvm);
+    rewriter.replaceOp(op, cloned->getResults());
+  }
+
+  // Insert terminator.
+  if (!hasTerminator) {
+    rewriter.setInsertionPointToEnd(body);
+    rewriter.create<scf::YieldOp>(loc, toYield);
+  }
+
+  // Cleanup and replace.
+  rewriter.eraseOp(performConcurrentlyOp);
+  rewriter.replaceOp(inParallelOp, forOp.getResults());
+
+  return forOp;
 }
