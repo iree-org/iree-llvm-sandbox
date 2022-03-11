af5c24b521327f14d09a2bf37a8ea618715ee58c
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2022-03-11 18:17:25 +0100

    Add a smoke test for iree_dialects (#357)

--- python/examples/dialects/test.py
+++ python/examples/dialects/test.py
@@ -1,3 +1,7 @@
 import mlir.dialects.linalg_ext as linalg_ext
 
 assert hasattr(linalg_ext, 'YieldOp'), 'Could not load linalg_ext.YieldOp'
+
+from mlir.dialects import iree_linalg_ext
+assert hasattr(iree_linalg_ext,
+               'YieldOp'), 'Could not load iree_linalg_ext.YieldOp'
