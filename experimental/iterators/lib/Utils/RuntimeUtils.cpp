491331fcc000805d70cdf68e26976e6cdab85b9f
Ingo Müller <ingomueller@google.com>
2022-03-23 11:51:54 +0000

    Destroy iterator states with dedicated call per value.
    This mimics the behavior of C++ destructors, which are called at the end
    of the corresponding scope, and which is probably closer to what we
    eventually want to convert to.

--- experimental/iterators/lib/Utils/RuntimeUtils.cpp
+++ experimental/iterators/lib/Utils/RuntimeUtils.cpp
@@ -2,13 +2,11 @@
 
 #include "iterators/Utils/RuntimeUtils.h"
 
+#include <cstdint>
 #include <cstdio>
 #include <iostream>
-#include <list>
 #include <memory>
 #include <tuple>
-
-#include <llvm/ADT/Any.h>
 
 #include "iterators/Operators/ColumnScanOperator.h"
 #include "iterators/Operators/ReduceOperator.h"
@@ -21,36 +19,16 @@ using namespace mlir::iterators::utils;
 // Exposed C API.
 //===---------------------------------------------------------------------===//
 
-/// List of things that will be destructed after the query completed
-using Heap = std::list<llvm::Any>;
-
-/// Construct an object of type `T` on the given heap and return a pointer
-/// to the contructed object.
-template <typename T, typename... Types>
-T *emplaceHeap(int8_t *const heap, Types &&...args) {
-  auto const typedHeap = reinterpret_cast<Heap *>(heap);
-  // Wrap object into `std::shared_ptr` because `llvm::Any` is not copy-
-  // constructible
-  typedHeap->emplace_back(std::make_shared<T>(std::forward<Types>(args)...));
-  auto const ptr = llvm::any_cast<std::shared_ptr<T>>(typedHeap->back());
-  return ptr.get();
-}
-
-extern "C" RUNTIME_UTILS_EXPORT int8_t *iteratorsMakeHeap() {
-  auto const heap = new Heap();
-  return reinterpret_cast<int8_t *>(heap);
-}
-
-extern "C" RUNTIME_UTILS_EXPORT void iteratorsDestroyHeap(int8_t *const heap) {
-  auto const typedHeap = reinterpret_cast<Heap *>(heap);
-  delete typedHeap;
-}
-
-extern "C" int8_t *iteratorsMakeSampleInputOperator(int8_t *const heap) {
+extern "C" int8_t *iteratorsMakeSampleInputOperator() {
   std::vector<int32_t> sampleInput = {1, 2, 3};
   using ScanOperatorType = decltype(makeColumnScanOperator(sampleInput));
-  auto const ptr = emplaceHeap<ScanOperatorType>(heap, sampleInput);
-  return reinterpret_cast<int8_t *>(ptr);
+  return reinterpret_cast<int8_t *>(new ScanOperatorType(sampleInput));
+}
+
+extern "C" void iteratorsDestroySampleInputOperator(int8_t *const op) {
+  using ScanOperatorType = ColumnScanOperator<int32_t>;
+  auto *const typedOp = reinterpret_cast<ScanOperatorType *>(op);
+  delete typedOp;
 }
 
 // This needs to be global because redefining it in two different places yields
@@ -59,15 +37,18 @@ auto const sum = [](std::tuple<int32_t> t1, std::tuple<int32_t> t2) {
   return std::make_tuple(std::get<0>(t1) + std::get<0>(t2));
 };
 
-extern "C" int8_t *iteratorsMakeReduceOperator(int8_t *const heap,
-                                               int8_t *const upstream) {
-  using ScanOperatorType =
-      decltype(makeColumnScanOperator(std::declval<std::vector<int32_t>>()));
-  using ReduceOperatorType =
-      decltype(makeReduceOperator(std::declval<ScanOperatorType *>(), sum));
-  auto const ptr = emplaceHeap<ReduceOperatorType>(
-      heap, reinterpret_cast<ScanOperatorType *>(upstream), sum);
-  return reinterpret_cast<int8_t *>(ptr);
+extern "C" int8_t *iteratorsMakeReduceOperator(int8_t *const upstream) {
+  using ScanOperatorType = ColumnScanOperator<int32_t>;
+  using ReduceOperatorType = ReduceOperator<ScanOperatorType, decltype(sum)>;
+  return reinterpret_cast<int8_t *>(new ReduceOperatorType(
+      reinterpret_cast<ScanOperatorType *>(upstream), sum));
+}
+
+extern "C" void iteratorsDestroyReduceOperator(int8_t *const op) {
+  using ScanOperatorType = ColumnScanOperator<int32_t>;
+  using ReduceOperatorType = ReduceOperator<ScanOperatorType, decltype(sum)>;
+  auto *const typedOp = reinterpret_cast<ReduceOperatorType *>(op);
+  delete typedOp;
 }
 
 extern "C" void iteratorsComsumeAndPrint(int8_t *const upstream) {
--- experimental/iterators/test/simple-plan-target.mlir
+++ experimental/iterators/test/simple-plan-target.mlir
@@ -3,18 +3,19 @@
 // RUN:   -shared-libs=%mlir_runner_utils_dir/libruntime_utils%shlibext \
 // RUN: | FileCheck %s
 
-func private @iteratorsMakeHeap() -> (!llvm.ptr<i8>)
-func private @iteratorsDestroyHeap(!llvm.ptr<i8>) -> ()
-func private @iteratorsMakeSampleInputOperator(!llvm.ptr<i8>) -> !llvm.ptr<i8>
-func private @iteratorsMakeReduceOperator(!llvm.ptr<i8>, !llvm.ptr<i8>) -> !llvm.ptr<i8>
+
 func private @iteratorsComsumeAndPrint(!llvm.ptr<i8>) -> ()
+func private @iteratorsDestroyReduceOperator(!llvm.ptr<i8>) -> ()
+func private @iteratorsDestroySampleInputOperator(!llvm.ptr<i8>) -> ()
+func private @iteratorsMakeReduceOperator(!llvm.ptr<i8>) -> !llvm.ptr<i8>
+func private @iteratorsMakeSampleInputOperator() -> !llvm.ptr<i8>
 
 func @main() {
-  %heap = call @iteratorsMakeHeap() : () -> (!llvm.ptr<i8>)
-  %input = call @iteratorsMakeSampleInputOperator(%heap) : (!llvm.ptr<i8>) -> (!llvm.ptr<i8>)
-  %reduce = call @iteratorsMakeReduceOperator(%heap, %input) : (!llvm.ptr<i8>, !llvm.ptr<i8>) -> (!llvm.ptr<i8>)
+  %input = call @iteratorsMakeSampleInputOperator() : () -> (!llvm.ptr<i8>)
+  %reduce = call @iteratorsMakeReduceOperator(%input) : (!llvm.ptr<i8>) -> (!llvm.ptr<i8>)
   // CHECK: (6)
   call @iteratorsComsumeAndPrint(%reduce) : (!llvm.ptr<i8>) -> ()
-  call @iteratorsDestroyHeap(%heap) : (!llvm.ptr<i8>) -> ()
+  call @iteratorsDestroyReduceOperator(%reduce) : (!llvm.ptr<i8>) -> ()
+  call @iteratorsDestroySampleInputOperator(%input) : (!llvm.ptr<i8>) -> ()
   return
 }
