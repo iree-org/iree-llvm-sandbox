3962916c529715b21281d31d3adbf0c0fb48caf6
Tobias Gysi <gysit@users.noreply.github.com>
2022-04-19 15:47:37 +0200

    Updated fusion benchmark. (#429)

--- python/examples/fusion/test.py
+++ python/examples/fusion/test.py
@@ -13,53 +13,66 @@ from .definitions import *
 ### Expert for running the fusion tests.
 ################################################################################
 
-
-# 1 linalg.fill -> linalg.matmul fusion.
 def fill_matmul_fusion():
   fun_name = 'matmul'
   op_name = 'linalg.matmul'
-  expert = Fuse(fun_name, op_name, tile_sizes=[8, 16, 0],          \
+  expert = Tile(fun_name, op_name, tile_sizes=[12, 32, 16],        \
                 tile_interchange=[0, 1, 2])                        \
-    .then(Pad(fun_name, 'linalg.fill',                             \
-              padding_values=[0.0, 0.0],                           \
-              padding_dimensions=[0, 1]))                          \
-    .then(Tile(fun_name, op_name, tile_sizes=[0, 0, 24]))          \
     .then(Pad(fun_name, op_name,                                   \
               padding_values=[0.0, 0.0, 0.0],                      \
               padding_dimensions=[0, 1, 2],                        \
-              pack_paddings=[1, 1, 0]))                            \
+              pack_paddings=[1, 1, 0],                             \
+              hoist_paddings=[2, 3, 0],                            \
+              transpose_paddings=[[0, 1], [0, 1], [0, 1]]))        \
     .then(Vectorize(fun_name, '', vectorize_paddings=True))        \
-    .then(LoweringOnlyExpert('', ''))
+    .then(Bufferize()) \
+    .then(LowerVectors(split_transfers='none')) \
+    .then(LowerToLLVM())
+
   keys = ['M', 'N', 'K']
-  n_iters = 1
-  problem_size_list = [[24, 32, 48], [27, 37, 43]]
+  n_iters = 1000
+  problem_size_list = [[61, 33, 54]]
   test_harness(lambda s, t: MatmulProblem(), [[np.float32] * 3],
                test_sizes(keys, problem_size_list),
                [expert.print_ir(after_all=False, at_begin=False, llvm=False)],
                n_iters=n_iters,
                function_name=fun_name,
-               zero_at_each_iteration=True)
+               zero_at_each_iteration=False)
 
 
 def fill_matmul_bias_add_fusion():
   fun_name = 'matmul_bias_add'
   op_name = 'linalg.generic'
-  # FIXME: Cannot pad and vectorize a generic consuming a for loop output.
-  expert = Fuse(fun_name, op_name, tile_sizes=[8, 16],               \
+  # FIXME: Split padding and hoist padding.
+  expert = Fuse(fun_name, op_name, tile_sizes=[12, 32],              \
                 tile_interchange=[0, 1])                             \
       .then(Pad(fun_name, 'linalg.fill',                             \
                 padding_values=[0.0, 0.0],                           \
                 padding_dimensions=[0, 1]))                          \
-      .then(Tile(fun_name, 'linalg.matmul', tile_sizes=[0, 0, 24]))  \
+      .then(Pad(fun_name, 'linalg.matmul',                           \
+                padding_values=[0.0, 0.0, 0.0],                      \
+                padding_dimensions=[0, 1]))                          \
+      .then(Pad(fun_name, op_name,                                   \
+                padding_values=[0., 0., 0.],                         \
+                padding_dimensions=[0, 1],                           \
+                pack_paddings=[0, 0],                                \
+                hoist_paddings=[2, 0]))                              \
+      .then(Tile(fun_name, 'linalg.matmul', tile_sizes=[0, 0, 16]))  \
       .then(Pad(fun_name, 'linalg.matmul',                           \
                 padding_values=[0., 0., 0.],                         \
-                padding_dimensions=[0, 1, 2],                        \
+                padding_dimensions=[2],                              \
                 pack_paddings=[1, 1, 0]))                            \
-      .then(LoweringOnlyExpert('', ''))
+      .then(Pad(fun_name, 'linalg.matmul',                           \
+                hoist_paddings=[2, 3, 0],                            \
+                transpose_paddings=[[0, 1], [0, 1], [0, 1]]))        \
+      .then(Vectorize(fun_name, '', vectorize_paddings=True))        \
+      .then(Bufferize()) \
+      .then(LowerVectors(split_transfers='none')) \
+      .then(LowerToLLVM())
 
   keys = ['M', 'N', 'K']
-  n_iters = 1
-  problem_size_list = [[24, 32, 48], [27, 37, 43]]
+  n_iters = 1000
+  problem_size_list = [[61, 33, 54]]
   test_harness(lambda s, t: MatmulBiasAddProblem(), [[np.float32] * 4],
                test_sizes(keys, problem_size_list),
                [expert.print_ir(after_all=False, at_begin=False, llvm=False)],
