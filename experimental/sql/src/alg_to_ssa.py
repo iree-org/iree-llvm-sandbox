f3e114af95f572cdb64f4509c7cd59337237b9d7
Michel Weber <michel.weber@inf.ethz.ch>
2022-04-26 10:11:57 +0200

    rename parent_op business

--- experimental/sql/src/alg_to_ssa.py
+++ experimental/sql/src/alg_to_ssa.py
@@ -16,9 +16,9 @@ import dialects.rel_ssa as RelSSA
 # algebra dialect to the relational SSA dialect. The current design has a parent
 # class `RelAlgRewriter` that contains functions used for several `Rewriter`s.
 # All other `Rewriter`s inherit from that class. To introduce SSA, the passes
-# insert the children, that should become operands, before the matched op, then
-# use the `added_operations_before` functionality of the rewriter to link the
-# operands to the new operation.
+# insert the children (and the their whole block), that should become operands,
+# before the matched op, then use the `added_operations_before` functionality of
+# the rewriter to link the operands to the new operation.
 
 
 @dataclass
@@ -42,12 +42,13 @@ class RelAlgRewriter(RewritePattern):
         return s.elt_type
     return None
 
-  def find_type_in_parent_op(self, name: str, parent_op: Operation):
+  def find_type_in_parent_op(self, name: str, op: Operation):
     """
     Crawls through all parent_ops until reaching either a ModuleOp, in which
     case the lookup failed or reaching an operation with an input bag, that the
     type can be looked up in.
     """
+    parent_op = op.parent_op()
     if isinstance(parent_op, ModuleOp):
       raise Exception(f"element not found in parent schema: {name}")
     if isinstance(parent_op, RelAlg.Select):
@@ -56,14 +57,14 @@ class RelAlgRewriter(RewritePattern):
       if type_:
         return type_
       raise Exception(f"element not found in parent schema: {name}")
-    return self.find_type_in_parent_op(name, parent_op.parent_op())
+    return self.find_type_in_parent_op(name, parent_op)
 
 
 #===------------------------------------------------------------------------===#
 # Expressions
 #===------------------------------------------------------------------------===#
 """
-All expression rewriters implicitely assume that the last operation in a lock is
+All expression rewriters implicitly assume that the last operation in a block is
 the one to be yielded.
 """
 
@@ -84,7 +85,7 @@ class ColumnRewriter(RelAlgRewriter):
 
   @op_type_rewrite_pattern
   def match_and_rewrite(self, op: RelAlg.Column, rewriter: PatternRewriter):
-    res_type = self.find_type_in_parent_op(op.col_name.data, op.parent_op())
+    res_type = self.find_type_in_parent_op(op.col_name.data, op)
     new_op = RelSSA.Column.get(op.col_name.data, res_type)
     rewriter.insert_op_before_matched_op([new_op])
     rewriter.erase_matched_op()
