c9e9ddccacecc9de1dd4075ffcbbef2911e84ef6
Michel Weber <55622065+webmiche@users.noreply.github.com>
2022-05-17 10:35:41 +0200

    SQL: MLIRConverter (#494)
    This patch adds the IteratorsMlirConverter, a child of the standard [MLIRConverter](https://github.com/xdslproject/xdsl/blob/main/src/xdsl/mlir_converter.py) of xDSL. The tests are currently not run on the CI due to an ongoing discussion on where to run them exactly.

---
+++ experimental/sql/tools/IteratorsMLIRConverter.py
@@ -0,0 +1,22 @@
+import mlir_iterators.ir as ir
+from mlir_iterators.dialects import iterators as it
+from xdsl.mlir_converter import MLIRConverter
+from xdsl.ir import Attribute
+from dialects.iterators import Stream
+
+# This MLIRConverter enriches the standard xdsl MLIRConverter
+# (https://github.com/xdslproject/xdsl/blob/main/src/xdsl/mlir_converter.py) by
+# adding the types of the iterators dialect
+# (https://github.com/google/iree-llvm-sandbox/tree/main/experimental/iterators).
+
+
+class IteratorsMLIRConverter(MLIRConverter):
+
+  def register_external_dialects(self):
+    it.register_dialect()
+    super().register_external_dialects()
+
+  def convert_type(self, typ: Attribute) -> ir.Type:
+    if isinstance(typ, Stream):
+      return it.StreamType.get(self.convert_type(typ.types))
+    return super().convert_type(typ)
