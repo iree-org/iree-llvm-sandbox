efe4113f99185c4f5b4300657db7d5000729aa0b
Michel Weber <michel.weber@inf.ethz.ch>
2022-06-28 12:29:45 +0200

    first version

--- experimental/sql/dialects/rel_ssa.py
+++ experimental/sql/dialects/rel_ssa.py
@@ -321,6 +321,21 @@ class Literal(Expression):
     return Literal.build(attributes={"value": val}, result_types=[res])
 
 
+@irdl_op_definition
+class And(Expression):
+  name = "rel_ssa.and"
+
+  lhs = OperandDef(Boolean)
+  rhs = OperandDef(Boolean)
+
+  result = ResultDef(Boolean)
+
+  @staticmethod
+  @builder
+  def get(lhs: Operation, rhs: Operation) -> 'And':
+    return And.build(operands=[lhs, rhs], result_types=[Boolean()])
+
+
 #===------------------------------------------------------------------------===#
 # Operators
 #===------------------------------------------------------------------------===#
@@ -494,3 +509,4 @@ class RelSSA:
     self.ctx.register_op(Compare)
     self.ctx.register_op(Column)
     self.ctx.register_op(Yield)
+    self.ctx.register_op(And)
