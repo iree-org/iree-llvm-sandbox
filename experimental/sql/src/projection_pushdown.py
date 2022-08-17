cf17dc78c5dfd05119b3e1d947672394dcf2016d
Michel Weber <michel.weber@inf.ethz.ch>
2022-08-17 13:24:02 +0200

    further work

--- experimental/sql/src/projection_pushdown.py
+++ experimental/sql/src/projection_pushdown.py
@@ -13,23 +13,99 @@ from xdsl.pattern_rewriter import (RewritePattern, GreedyRewritePatternApplier,
 
 import dialects.rel_alg as RelAlg
 
+# This file contains several rewrites on the rel_alg level, all of which concern
+# projections.
+
 
 @dataclass
-class ProjectionPushdownRewriter(RewritePattern):
+class ProjectionOptimizer(RewritePattern):
+
+  def flatten(self, l: list[list[str]]) -> list[str]:
+    return [elem for sublist in l for elem in sublist]
 
   def find_cols_in_expr(self, op: RelAlg.Expression) -> list[str]:
     """
-    Returns the list of cols that are used in this operation or its children.
+    Returns the list of cols that are used in this expression or its children.
     """
-    if isinstance(op, RelAlg.BinOp):
-      return self.find_cols_in_expr(op.lhs.op) + self.find_cols_in_expr(
-          op.rhs.op)
     if isinstance(op, RelAlg.Column):
       return [op.col_name.data]
-    if isinstance(op, RelAlg.Compare):
-      return self.find_cols_in_expr(op.left.op) + self.find_cols_in_expr(
-          op.right.op)
-    return []
+    else:
+      return self.flatten([self.find_cols_in_expr(r.op) for r in op.regions])
+
+  def find_cols_upstream(self, op: RelAlg.Operator) -> list[str]:
+    """
+    Returns the list of cols that are used in this operation or its parents. If
+    the list contains None, a ModuleOp (so the output) was hit, showing that all
+    columns need to be kept.
+    """
+    if isinstance(op, ModuleOp):
+      return [None]
+    if isinstance(op, RelAlg.Select):
+      return self.flatten([
+          self.find_cols_in_expr(o) for o in op.predicates.ops
+      ]) + self.find_cols_upstream(op.parent_op())
+    if isinstance(op, RelAlg.Project):
+      return self.flatten(
+          [self.find_cols_in_expr(o) for o in op.projections.ops])
+    if isinstance(op, RelAlg.Aggregate):
+      return [s.data for s in op.col_names.data]
+
+
+@dataclass
+class ProjectionInference(ProjectionOptimizer):
+
+  # This rewriter infers columns in front of all Operators (but projections and
+  # tables) that map away all columns that aren't needed by this Operator or
+  # further up the tree.
+
+  @op_type_rewrite_pattern
+  def match_and_rewrite(self, op: RelAlg.Operator, rewriter: PatternRewriter):
+    if isinstance(op, RelAlg.Project) or isinstance(
+        op, RelAlg.Table) or isinstance(op, RelAlg.SchemaElement):
+      return
+    cols = list(dict.fromkeys(self.find_cols_upstream(op)))
+    if cols.__contains__(None):
+      return
+
+    new_proj = RelAlg.Project.get(
+        rewriter.move_region_contents_to_new_regions(op.input),
+        Region.from_operation_list([RelAlg.Column.get(s) for s in cols]),
+        ArrayAttr.from_list([StringAttr.from_str(s) for s in cols]))
+
+    if isinstance(op, RelAlg.Select):
+      rewriter.replace_matched_op(
+          RelAlg.Select.get(
+              Region.from_operation_list([new_proj]),
+              rewriter.move_region_contents_to_new_regions(op.predicates)))
+    elif isinstance(op, RelAlg.Aggregate):
+      rewriter.replace_matched_op(
+          RelAlg.Aggregate.get(Region.from_operation_list([new_proj]),
+                               [s.data for s in op.col_names.data],
+                               [s.data for s in op.functions.data],
+                               [s.data for s in op.res_names.data]))
+
+
+@dataclass
+class IdentityProjectionRemover(ProjectionOptimizer):
+
+  @op_type_rewrite_pattern
+  def match_and_rewrite(self, op: RelAlg.Project, rewriter: PatternRewriter):
+
+    # If any of the expressions in the projections list is not a Column
+    # expression, this is not an IdentityProjection.
+    # TODO: Maybe optimize simple expressions paterns (like add 0 or mult 1)
+    if not all([isinstance(o, RelAlg.Column) for o in op.projections.ops]):
+      return
+
+    cols = self.flatten([self.find_cols_in_expr(o) for o in op.projections.ops])
+
+    if cols == [s.data for s in op.names.data]:
+      new_op = op.input.blocks[0].detach_op(op.input.op)
+      rewriter.replace_matched_op(new_op)
+
+
+@dataclass
+class PushThroughSelect(ProjectionOptimizer):
 
   @op_type_rewrite_pattern
   def match_and_rewrite(self, op: RelAlg.Project, rewriter: PatternRewriter):
@@ -65,22 +141,24 @@ class ProjectionPushdownRewriter(RewritePattern):
           Region.from_operation_list([new_proj]),
           rewriter.move_region_contents_to_new_regions(op.input.op.predicates))
 
-      # If the output of the select has the same schema as the initial project,
-      # we don't need a second project.
-      if set(result_cols) == set(select_cols):
-        rewriter.replace_matched_op(new_sel)
-      else:
-        new_proj2 = RelAlg.Project.get(
-            Region.from_operation_list([new_sel]),
-            rewriter.move_region_contents_to_new_regions(op.projections),
-            ArrayAttr.from_list([StringAttr.from_str(s) for s in result_cols]))
-        rewriter.replace_matched_op(new_proj2)
+      new_proj2 = RelAlg.Project.get(
+          Region.from_operation_list([new_sel]),
+          rewriter.move_region_contents_to_new_regions(op.projections),
+          ArrayAttr.from_list([StringAttr.from_str(s) for s in result_cols]))
+      rewriter.replace_matched_op(new_proj2)
 
 
 def projection_pushdown(ctx: MLContext, query: ModuleOp):
-  walker = PatternRewriteWalker(GreedyRewritePatternApplier(
-      [ProjectionPushdownRewriter()]),
-                                walk_regions_first=False,
-                                apply_recursively=False,
-                                walk_reverse=False)
-  walker.rewrite_module(query)
+  infer_projections_walker = PatternRewriteWalker(GreedyRewritePatternApplier(
+      [ProjectionInference()]),
+                                                  walk_regions_first=False,
+                                                  apply_recursively=False,
+                                                  walk_reverse=False)
+  infer_projections_walker.rewrite_module(query)
+
+  identity_projection_walker = PatternRewriteWalker(GreedyRewritePatternApplier(
+      [IdentityProjectionRemover()]),
+                                                    walk_regions_first=False,
+                                                    apply_recursively=False,
+                                                    walk_reverse=False)
+  identity_projection_walker.rewrite_module(query)
