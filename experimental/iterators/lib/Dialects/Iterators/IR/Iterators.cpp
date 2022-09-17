0c0736dd0e98031296d1c4efa2c47f968ce6be8b
Ingo Müller <ingomueller@google.com>
2022-09-17 10:48:36 +0000

    Delete IR on "low-level iterators" and related tests.
    This IR was an early design attempt for lowering iterators to LLVM by
    providing an intermediate level of abstraction. This was eventually
    abandonded in favor of the current approach, which lowers to LLVM
    functions and structs directly. While that worked, it is becoming more
    and more clear that it bridges too high a gap, so we may want to
    introduce such an intermediate level again at some point.

--- experimental/iterators/lib/Dialects/Iterators/IR/Iterators.cpp
+++ experimental/iterators/lib/Dialects/Iterators/IR/Iterators.cpp
@@ -47,41 +47,6 @@ void IteratorsDialect::initialize() {
 #define GET_OP_CLASSES
 #include "iterators/Dialect/Iterators/IR/IteratorsOps.cpp.inc"
 
-LogicalResult CreateSampleInputStateOp::verify() {
-  IteratorInterface iteratorType =
-      createdState().getType().dyn_cast<IteratorInterface>();
-  assert(iteratorType);
-
-  TupleType tupleType =
-      TupleType::get(getContext(), {IntegerType::get(getContext(), 32)});
-  if (iteratorType.getElementType() != tupleType) {
-    return emitOpError() << "Type mismatch: Sample input iterator (currently) "
-                            "has to return elements of type 'tuple<i32>'";
-  }
-
-  return success();
-}
-
-LogicalResult CreateReduceStateOp::verify() {
-  IteratorInterface iteratorType =
-      createdState().getType().dyn_cast<IteratorInterface>();
-  assert(iteratorType);
-
-  IteratorInterface upstreamIteratorType =
-      upstreamState().getType().dyn_cast<IteratorInterface>();
-  assert(upstreamIteratorType);
-
-  if (iteratorType.getElementType() != upstreamIteratorType.getElementType()) {
-    return emitOpError() << "Type mismatch: Upstream iterator of reduce "
-                            "iterator must produce elements of type "
-                         << iteratorType.getElementType()
-                         << " but produces elements of type "
-                         << upstreamIteratorType.getElementType();
-  }
-
-  return success();
-}
-
 //===----------------------------------------------------------------------===//
 // Iterators types
 //===----------------------------------------------------------------------===//
