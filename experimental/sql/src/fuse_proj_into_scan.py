32905f2fa29219a70e13bd39c0d3f5de50197c07
Michel Weber <michel.weber@inf.ethz.ch>
2022-10-06 16:58:10 +0200

    update fuse proj into scan

--- experimental/sql/src/fuse_proj_into_scan.py
+++ experimental/sql/src/fuse_proj_into_scan.py
@@ -25,29 +25,45 @@ class ProjScanFuser(RewritePattern):
     if not isinstance(op.input.op, RelImpl.FullTableScanOp):
       return
 
-    # This rewrite expects that the projection only maps onto a subset of the
-    # columns of the input. Therefore, we first check that only IndexByName is
-    # present and then that there are equally as many cols that are yielded as
-    # there are in the result.
-    if not all([
-        isinstance(o.op, RelImpl.IndexByName) for o in op.projection.ops[-1].ops
-    ]):
-      return
-
     assert isinstance(op.projection.ops[-1], RelImpl.YieldTuple)
     yielded_ssa_values = op.projection.ops[-1].ops
 
-    col_names = [o.op.col_name.data for o in yielded_ssa_values]
-    res_names = [s.elt_name.data for s in op.input.typ.schema.data]
-    if len(col_names) == len(res_names):
+    # Get the list of used columns. The dict magic removes duplicates.
+    used_cols = list(
+        dict.fromkeys([
+            o.col_name.data
+            for o in op.projection.ops
+            if isinstance(o, RelImpl.IndexByName)
+        ]))
+    input_names = [s.elt_name.data for s in op.input.typ.schema.data]
+
+    # If all the input cols are used, there is nothing to fuse.
+    if len(used_cols) == len(input_names):
       return
 
     col_types = [o.typ for o in yielded_ssa_values]
 
     new_op = RelImpl.FullTableScanOp.get(op.input.op.table_name.data,
-                                         RelImpl.Bag.get(col_types, col_names),
-                                         col_names)
-    rewriter.replace_matched_op(new_op)
+                                         RelImpl.Bag.get(col_types, used_cols),
+                                         used_cols)
+
+    if all([
+        isinstance(o.op, RelImpl.IndexByName) for o in op.projection.ops[-1].ops
+    ]):
+      # If the projection is simple, replace the projection by a partial load.
+      rewriter.replace_matched_op(new_op)
+    else:
+      # For a complex projection, replace the projection with a projection
+      # reading from the partial load.
+      new_reg = rewriter.move_region_contents_to_new_regions(op.projection)
+      rewriter.modify_block_argument_type(
+          new_reg.blocks[0].args[0],
+          RelImpl.Tuple.get(
+              col_types,
+              [s.elt_name.data for s in new_op.result.typ.schema.data]))
+      rewriter.insert_op_before_matched_op(new_op)
+      rewriter.replace_matched_op(
+          RelImpl.Project.from_result_type(new_op, op.result.typ, new_reg))
 
     if len(op.input.uses) == 0:
       # TODO: remove that safe_erase is False, when we have some kind of dead code
