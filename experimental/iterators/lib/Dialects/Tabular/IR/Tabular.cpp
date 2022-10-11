5759e27cd9bb1e5c2c7de3faa1b1df1e1dd8424c
Ingo Müller <ingomueller@google.com>
2022-10-11 10:54:26 +0200

    Iterators: Add tabular.view_as_tabular op (#594)

--- experimental/iterators/lib/Dialects/Tabular/IR/Tabular.cpp
+++ experimental/iterators/lib/Dialects/Tabular/IR/Tabular.cpp
@@ -47,6 +47,48 @@ void TabularDialect::initialize() {
 #define GET_OP_CLASSES
 #include "iterators/Dialect/Tabular/IR/TabularOps.cpp.inc"
 
+LogicalResult ViewAsTabularOp::verify() {
+  auto viewType = view().getType().cast<TabularViewType>();
+  TypeRange columnTypes = viewType.getColumnTypes();
+
+  // Verify matching column/element types.
+  if (columnTypes.size() != memrefs().size()) {
+    return emitOpError()
+           << "type mismatch: should return a tabular view with the same "
+           << "number of columns as the number of input memrefs (expected: "
+           << memrefs().size() << ", found: " << columnTypes.size() << ").";
+  }
+  for (size_t i = 0; i < columnTypes.size(); i++) {
+    Type memrefElementType =
+        memrefs().getTypes()[i].cast<MemRefType>().getElementType();
+    if (memrefElementType != columnTypes[i]) {
+      return emitOpError()
+             << "type mismatch: returned tabular view has column type "
+             << columnTypes[i] << " at index " << i << " but should have type "
+             << memrefElementType << ", the element type of the memref at the "
+             << "same index.";
+    }
+  }
+
+  // Verify all memrefs are of equal static length.
+  if (!llvm::is_splat(llvm::map_range(memrefs().getTypes(), [](Type t) {
+        return t.cast<MemRefType>().getDimSize(0);
+      }))) {
+    std::string lengths;
+    {
+      llvm::raw_string_ostream stream(lengths);
+      llvm::interleaveComma(memrefs().getTypes(), stream, [&](Type type) {
+        stream << type.cast<MemRefType>().getDimSize(0);
+      });
+    }
+    return emitOpError()
+           << "type mismatch: input memrefs cannot have different static "
+           << "shapes (sizes found for dimension 0: " << lengths << ").";
+  }
+
+  return success();
+}
+
 //===----------------------------------------------------------------------===//
 // Tabular types
 //===----------------------------------------------------------------------===//
