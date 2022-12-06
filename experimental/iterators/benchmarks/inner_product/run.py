ada8b9c781b68094a16315275d9150a3cea42a1c
Ingo Müller <ingomueller@google.com>
2022-12-06 11:28:12 +0100

    Iterators: Extend benchmarks and plot scripts to throughput of tight loops (#625)
    * Report times as ns instead of us.
    * Allow for running experiment multiple times in tight loop.
    * Make run.sh choose repetitions for a minimum of 2^25 total ops.
    * Build upstream dialects into Python module.
    * Implement jitted repetition loop for iterators method.
    * [NFC] Rename plot_time_by_method_dtype.
    * Add plot for processing throughput.
    * Realign input data to cache line size.

--- experimental/iterators/benchmarks/inner_product/run.py
+++ experimental/iterators/benchmarks/inner_product/run.py
@@ -11,21 +11,83 @@ import time
 import numpy as np
 import pandas as pd
 
+from iree.compiler.runtime.np_to_memref import get_ranked_memref_descriptor
+from mlir.sandbox.utils import realign
 from mlir_iterators.dialects import iterators as it
 from mlir_iterators.dialects import tabular as tab
+from mlir_iterators.dialects import arith, func, memref, scf
 from mlir_iterators.execution_engine import ExecutionEngine
-from mlir_iterators.ir import Context, Module
+from mlir_iterators.ir import (
+    Context,  # (Comment preserves formatting.)
+    DictAttr,
+    IndexType,
+    IntegerType,
+    InsertionPoint,
+    Location,
+    MemRefType,
+    Module,
+    SymbolTable,
+    UnitAttr,
+)
 from mlir_iterators.passmanager import PassManager
 from mlir_iterators.runtime.pandas_to_iterators import to_tabular_view_descriptor
 import mlir_iterators.all_passes_registration
+
+_MLIR_RUNNER_UTILS_LIB_ENV = "MLIR_RUNNER_UTILS_LIB"
+_MLIR_RUNNER_UTILS_LIB_DEFAULT = "libmlir_runner_utils.so"
+_MLIR_C_RUNNER_UTILS_LIB_ENV = "MLIR_C_RUNNER_UTILS_LIB"
+_MLIR_C_RUNNER_UTILS_LIB_DEFAULT = "libmlir_c_runner_utils.so"
+
+
+# Copied from mlir.sandbox.compilation. That package uses the vanilla `mlir`
+# package instead of `mlir_iterators` as the rest of this file, so they are
+# incompatible.
+def emit_benchmarking_function(name: str, bench: func.FuncOp) -> func.FuncOp:
+  """Produces the benchmarking function.
+
+  This function calls the given function `bench` as many times as requested by
+  its last argument.
+  """
+  i64_type = IntegerType.get_signless(64)
+  nano_time = func.FuncOp("nanoTime", ([], [i64_type]), visibility="private")
+  nano_time.attributes["llvm.emit_c_interface"] = UnitAttr.get()
+
+  memref_of_i64_type = MemRefType.get([-1], i64_type)
+  wrapper = func.FuncOp(
+      # Same signature and an extra buffer of indices to save timings.
+      name,
+      (bench.arguments.types + [memref_of_i64_type], bench.type.results),
+      visibility="public")
+  wrapper.attributes["llvm.emit_c_interface"] = UnitAttr.get()
+
+  num_results = len(bench.type.results)
+  with InsertionPoint(wrapper.add_entry_block()):
+    timer_buffer = wrapper.arguments[-1]
+    zero = arith.ConstantOp.create_index(0)
+    n_iterations = memref.DimOp(IndexType.get(), timer_buffer, zero)
+    one = arith.ConstantOp.create_index(1)
+    iter_args = list(wrapper.arguments[-num_results - 1:-1])
+    loop = scf.ForOp(zero, n_iterations, one, iter_args)
+    with InsertionPoint(loop.body):
+      start = func.CallOp(nano_time, [])
+      args = list(wrapper.arguments[:-num_results - 1])
+      args.extend(loop.inner_iter_args)
+      call = func.CallOp(bench, args)
+      end = func.CallOp(nano_time, [])
+      time = arith.SubIOp(end, start)
+      memref.StoreOp(time, timer_buffer, [loop.induction_variable])
+      scf.YieldOp(list(call.results))
+    func.ReturnOp(loop)
+
+  return wrapper
 
 
 def setup_data(num_elements, dtype):
   """Sets up the input data: two `numpy.arrays` with `num_elements` each of
   type `dtype`."""
 
-  a = np.arange(num_elements, dtype=dtype)
-  b = np.arange(num_elements, dtype=dtype)
+  a = realign(np.arange(num_elements, dtype=dtype))
+  b = realign(np.arange(num_elements, dtype=dtype))
   return a, b
 
 
@@ -60,9 +122,27 @@ class Method(ABC):
     pass
 
   @abstractmethod
-  def run(self, inputs):
-    """Run the benchmark on the previously prepared data."""
+  def run_once(self, inputs):
+    """Run the benchmark on the previously prepared data once."""
     pass
+
+  def run(self, inputs, num_repetitions):
+    """Run the benchmark on the previously prepared data num_repetitions times.
+    Implementations may overload this function to run the repetitions in a tight
+    loop."""
+    run_times_ns = []
+    results = []
+    for _ in range(num_repetitions):
+      start = time.time()
+      result = self.run_once(inputs)
+      end = time.time()
+
+      run_time_s = end - start
+      run_time_ns = int(run_time_s * 10**9)
+      run_times_ns.append(run_time_ns)
+      results.append(result)
+
+    return run_times_ns, results
 
 
 class NumpyMethod(Method):
@@ -74,7 +154,7 @@ class NumpyMethod(Method):
   def name(cls):
     return 'numpy'
 
-  def run(self, inputs):
+  def run_once(self, inputs):
     a, b = inputs
     return np.dot(a, b).item()
 
@@ -124,31 +204,56 @@ class IteratorsMethod(Method):
     return code
 
   def compile(self):
-    with Context():
+    with Context(), Location.unknown():
       it.register_dialect()
       tab.register_dialect()
       code = self._load_code()
       mod = Module.parse(code)
+      symbol_table = SymbolTable(mod.operation)
+      main_func = symbol_table['main']
+      with InsertionPoint(mod.body):
+        emit_benchmarking_function('main_bench', main_func)
       pm = PassManager.parse(  # (Comment for better formatting.)
           'convert-iterators-to-llvm,'
           'convert-states-to-llvm,'
           'convert-memref-to-llvm,'
+          'convert-scf-to-cf,'
           'convert-func-to-llvm,'
           'reconcile-unrealized-casts,'
-          'convert-scf-to-cf,'
           'convert-cf-to-llvm')
     pm.run(mod)
-    self.engine = ExecutionEngine(mod, opt_level=3)
+    shared_libs = [
+        os.getenv(_MLIR_RUNNER_UTILS_LIB_ENV, _MLIR_RUNNER_UTILS_LIB_DEFAULT),
+        os.getenv(_MLIR_C_RUNNER_UTILS_LIB_ENV,
+                  _MLIR_C_RUNNER_UTILS_LIB_DEFAULT)
+    ]
+    self.engine = ExecutionEngine(mod, shared_libs=shared_libs, opt_level=3)
 
     # Invoke once to move set-up time out of run time.
-    self.run(self.sample_input)
+    self.run(self.sample_input, num_repetitions=1)
 
-  def run(self, inputs):
+  def run_once(self, inputs):
     ctypes_class = np.ctypeslib.as_ctypes_type(self.dtype)
     result = ctypes_class(-1)
-    pointer = ctypes.pointer(result)
-    self.engine.invoke('main', inputs, ctypes.pointer(pointer))
+    result_ptr = ctypes.pointer(result)
+    self.engine.invoke('main', inputs, ctypes.pointer(result_ptr))
     return result.value
+
+  def run(self, inputs, num_repetitions):
+    ctypes_class = np.ctypeslib.as_ctypes_type(self.dtype)
+    result = ctypes_class(-1)
+    result_ptr = ctypes.pointer(result)
+
+    timings = np.empty([num_repetitions], dtype=np.int64)
+    timings_desc = get_ranked_memref_descriptor(timings)
+
+    self.engine.invoke('main_bench', inputs, ctypes.pointer(result_ptr),
+                       ctypes.pointer(ctypes.pointer(timings_desc)))
+
+    # Assume deterministic results for simplicity.
+    results = [result.value] * num_repetitions
+
+    return timings.tolist(), results
 
 
 # Registry of methods that can be benchmarked.
@@ -164,6 +269,12 @@ def parse_args():
 
   parser = argparse.ArgumentParser(
       description='Run benchmark computing inner product of two vectors.')
+  parser.add_argument('-r',
+                      '--num-repetitions',
+                      metavar='N',
+                      type=int,
+                      default=1,
+                      help='Number of repetitions in immediate succession.')
   parser.add_argument('-n',
                       '--num-elements',
                       metavar='N',
@@ -208,23 +319,24 @@ def main():
 
   # Run computation.
   start = time.time()
-  res = method.run(inputs)
+  run_times_ns, results = method.run(inputs, args.num_repetitions)
   end = time.time()
-  run_time_s = end - start
+  total_run_time_s = end - start
 
   # Assemble and print benchmark data.
-  prepare_time_us = int(prepare_time_s * 10**6)
-  compile_time_us = int(compile_time_s * 10**6)
-  run_time_us = int(run_time_s * 10**6)
+  prepare_time_ns = int(prepare_time_s * 10**9)
+  compile_time_ns = int(compile_time_s * 10**9)
+  total_run_time_ns = int(total_run_time_s * 10**9)
 
   data = {
       'method': method.name,
       'dtype': dtype.name,
       'num_elements': num_elements,
-      'run_time_us': run_time_us,
-      'prepare_time_us': prepare_time_us,
-      'compile_time_us': compile_time_us,
-      'result': str(res),
+      'total_run_time_ns': total_run_time_ns,
+      'run_times_ns': run_times_ns,
+      'prepare_time_ns': prepare_time_ns,
+      'compile_time_ns': compile_time_ns,
+      'results': results,
       'datetime': datetime.now().isoformat(),
   }
 
--- experimental/iterators/benchmarks/inner_product/run.sh
+++ experimental/iterators/benchmarks/inner_product/run.sh
@@ -4,7 +4,7 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 RUN_SCRIPT="${SOURCE_DIR}/run.py"
 
-NUM_ELEMENTS=($(for l in {0..25}; do echo $((2**l)); done))
+NUM_ELEMENTS=($(for l in {8..25}; do echo $((2**l)); done))
 DTYPES=(int8 int16 int32 int64 float32 float64)
 METHODS=(numpy iterators)
 
@@ -14,13 +14,15 @@ METHODS=(numpy iterators)
 exhaust() {(
   for n in ${NUM_ELEMENTS[@]}
   do
+    # Compute number of repetitions as floor(total_num_ops/n).
+    r=$((($total_num_ops + $n - 1) / $n))
     for t in ${DTYPES[@]}
     do
       for m in ${METHODS[@]}
       do
         for _ in $(seq $num_repetitions)
         do
-          "$RUN_SCRIPT" -n $n -t $t -m $m
+          "$RUN_SCRIPT" -n $n -t $t -m $m -r $r
         done
       done
     done
@@ -33,20 +35,14 @@ exhaust() {(
 test() {(
   for n in ${NUM_ELEMENTS[@]}
   do
-    for _ in 1 2  # Run twice for stddev to make sense
-    do
-      "$RUN_SCRIPT" -n $n
-    done
+    "$RUN_SCRIPT" -n $n -r 2  # Run twice for stddev to make sense
   done
 
   for t in ${DTYPES[@]}
   do
     for m in ${METHODS[@]}
     do
-      for _ in 1 2  # Run twice for stddev to make sense
-      do
-        "$RUN_SCRIPT" -t $t -m $m
-      done
+      "$RUN_SCRIPT" -t $t -m $m -r 2   # Run twice for stddev to make sense
     done
   done
 )}
@@ -61,15 +57,19 @@ print_usage() {
 
 outfile="${SOURCE_DIR}/result.jsonl"
 num_repetitions=10
+total_num_ops=$((2**25))
 
 # Parse options.
-while getopts ":o:r:" o; do
+while getopts ":o:r:t:" o; do
   case "${o}" in
     o)
       outfile=${OPTARG}
       ;;
     r)
       num_repetitions=${OPTARG}
+      ;;
+    t)
+      total_num_ops=${OPTARG}
       ;;
     *)
       print_usage
