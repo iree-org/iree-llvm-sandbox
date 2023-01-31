36ab9f0257b73e49952f7f6e5af0e5ec3dfff052
Ingo Müller <ingomueller@google.com>
2023-01-31 09:37:49 +0000

    Remove our MLIRSupport library, which is now empty.

--- experimental/iterators/lib/Utils/MLIRSupport.cpp
+++
@@ -1,9 +0,0 @@
-//===-- MLIRSupport.cpp - Utils for dealing with MLIR -----------*- C++ -*-===//
-//
-// Licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
-#include "iterators/Utils/MLIRSupport.h"
