4ff44004777f26c95acd7d94641a45c271be11bd
Ingo Müller <ingomueller@google.com>
2023-03-10 01:48:46 +0000

    Create transformation that decomposes tuples into its elements.

--- experimental/iterators/lib/Dialect/Tuple/Transforms/DecomposeTuples.cpp
+++ experimental/iterators/lib/Dialect/Tuple/Transforms/DecomposeTuples.cpp
@@ -11,8 +11,11 @@
 
 #include "iterators/Dialect/Tuple/IR/Tuple.h"
 #include "iterators/Dialect/Tuple/Transforms/Passes.h"
-#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/Func/Transforms/OneToNFuncConversions.h"
+#include "mlir/Dialect/SCF/Transforms/Transforms.h"
 #include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/OneToNTypeConversion.h"
+#include "llvm/ADT/SmallVector.h"
 
 namespace mlir {
 #define GEN_PASS_CLASSES
@@ -21,19 +24,179 @@ namespace mlir {
 
 using namespace mlir;
 using namespace mlir::iterators;
+using namespace mlir::tuple;
+
+using InputMapping = OneToNTypeMapping::InputMapping;
+
+class DecomposeTuplesTypeConverter : public OneToNTypeConverter {
+public:
+  DecomposeTuplesTypeConverter() {
+    addConversion([](Type type) { return type; });
+    addConversion([](TupleType type, SmallVectorImpl<Type> &results) {
+      type.getFlattenedTypes(results);
+      return success();
+    });
+  }
+};
+
+class DecomposeFromElementsOp
+    : public OneToNOpConversionPattern<FromElementsOp> {
+public:
+  using OneToNOpConversionPattern<FromElementsOp>::OneToNOpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(FromElementsOp op, OpAdaptor adaptor,
+                  OneToNPatternRewriter &rewriter) const override {
+    // Simply forward converted operands.
+    rewriter.replaceOp(op, adaptor.getFlatOperands(),
+                       adaptor.getResultMapping());
+
+    return success();
+  }
+};
+
+class DecomposeToElementsOp : public OneToNOpConversionPattern<ToElementsOp> {
+public:
+  using OneToNOpConversionPattern<ToElementsOp>::OneToNOpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ToElementsOp op, OpAdaptor adaptor,
+                  OneToNPatternRewriter &rewriter) const override {
+    // Simply forward converted operands.
+    rewriter.replaceOp(op, adaptor.getFlatOperands(),
+                       adaptor.getResultMapping());
+
+    return success();
+  }
+};
 
 void iterators::populateDecomposeTuplesPatterns(TypeConverter &typeConverter,
                                                 RewritePatternSet &patterns) {
   patterns.add<
       // clang-format off
+      DecomposeFromElementsOp,
+      DecomposeToElementsOp
       // clang-format on
       >(typeConverter, patterns.getContext());
+}
+
+/// Creates IR that builds FromElementsOps to assemble a value of the given,
+/// portentially recursive tuple type from the given range of inputs. This can
+/// be used as argument and source materializations for tuple decomposition.
+static std::optional<Value> buildFromElementsOp(OpBuilder &builder,
+                                                TypeConverter &typeConverter,
+                                                Type type, ValueRange inputs,
+                                                Location loc) {
+  auto tupleType = type.dyn_cast<TupleType>();
+  if (!tupleType)
+    return {};
+
+  // Create mapping for elements of inputs.
+  TypeRange originalInputTypes = tupleType.getTypes();
+  OneToNTypeMapping inputMapping(originalInputTypes);
+  if (failed(
+          typeConverter.convertSignatureArgs(originalInputTypes, inputMapping)))
+    return {};
+
+  // Assemble element values at this nesting level.
+  SmallVector<Value> operands;
+  for (auto [i, elementType] : llvm::enumerate(originalInputTypes)) {
+    // Element is a nested tuple: recursively build back the tuple.
+    ValueRange elementValues = inputMapping.getConvertedValues(inputs, i);
+    if (elementType.isa<TupleType>()) {
+      std::optional<Value> createdTuple = buildFromElementsOp(
+          builder, typeConverter, elementType, elementValues, loc);
+      if (!createdTuple.has_value() || !createdTuple.value())
+        return {};
+      operands.push_back(createdTuple.value());
+      continue;
+    }
+
+    // Any other type: take as is.
+    assert(elementValues.size() == 1);
+    Value operand = elementValues.front();
+    operands.push_back(operand);
+  }
+
+  // Build the tuple from its elements.
+  auto createStateOp = builder.create<FromElementsOp>(loc, type, operands);
+  assert(createStateOp->getNumResults() == 1);
+  return createStateOp->getResult(0);
+}
+
+/// Creates IR that extracts the elements of the given input tuple recursively
+/// using ToElementOps. This can be used as target conversion for tuple
+/// decomposition.
+std::optional<SmallVector<Value>>
+buildToElementsOp(OpBuilder &builder, TypeConverter &typeConverter,
+                  TypeRange resultTypes, Value input, Location loc) {
+  auto tupleType = input.getType().dyn_cast<TupleType>();
+  if (!tupleType)
+    return {};
+
+  // Create mapping for elements of inputs.
+  TypeRange originalElementTypes = tupleType.getTypes();
+  OneToNTypeMapping elementMapping(originalElementTypes);
+  if (failed(typeConverter.convertSignatureArgs(originalElementTypes,
+                                                elementMapping)))
+    return {};
+
+  // Extract elements at this level.
+  auto toElements =
+      builder.create<ToElementsOp>(loc, originalElementTypes, input);
+
+  // Assemble final values, recursing where necessary.
+  SmallVector<Value> results;
+  for (auto [i, nestedElement] : llvm::enumerate(toElements.getResults())) {
+    // Nested element is a tuple: use results of recursive call.
+    if (nestedElement.getType().isa<TupleType>()) {
+      TypeRange nestedResultTypes = elementMapping.getConvertedTypes(i);
+      std::optional<SmallVector<Value>> nestedValues = buildToElementsOp(
+          builder, typeConverter, nestedResultTypes, nestedElement, loc);
+      if (!nestedValues.has_value())
+        return {};
+      results.append(nestedValues->begin(), nestedValues->end());
+    } else {
+      // Any other element: use as is.
+      results.push_back(nestedElement);
+    }
+  }
+
+  return results;
 }
 
 struct DecomposeTuplesPass : public DecomposeTuplesBase<DecomposeTuplesPass> {
   void runOnOperation() override {
     ModuleOp module = getOperation();
     MLIRContext *context = &getContext();
+
+    // Assemble type convert with materializations.
+    DecomposeTuplesTypeConverter typeConverter;
+    auto buildFromElementsOpHelper = [&](OpBuilder &builder, Type type,
+                                         ValueRange inputs, Location loc) {
+      return buildFromElementsOp(builder, typeConverter, type, inputs, loc);
+    };
+    typeConverter.addArgumentMaterialization(buildFromElementsOpHelper);
+    typeConverter.addSourceMaterialization(buildFromElementsOpHelper);
+    auto buildToElementsOpHelper = [&](OpBuilder &builder,
+                                       TypeRange resultTypes, Value input,
+                                       Location loc) {
+      return buildToElementsOp(builder, typeConverter, resultTypes, input, loc);
+    };
+    typeConverter.addTargetMaterialization(buildToElementsOpHelper);
+
+    // Assemble patterns.
+    RewritePatternSet patterns(context);
+    populateDecomposeTuplesPatterns(typeConverter, patterns);
+    if (convertFuncOps)
+      populateFuncTypeConversionPatterns(typeConverter, patterns);
+    if (convertSCFOps)
+      scf::populateSCFStructuralOneToNTypeConversions(typeConverter, patterns);
+
+    // Run conversion.
+    if (failed(applyPartialOneToNConversion(module, typeConverter,
+                                            std::move(patterns))))
+      return signalPassFailure();
   };
 };
 
---
+++ experimental/iterators/test/Dialect/Tuple/Transforms/decompose-tuples.mlir
@@ -0,0 +1,95 @@
+// RUN: iterators-opt %s -decompose-tuples \
+// RUN: | FileCheck --enable-var-scope %s
+
+// RUN: iterators-opt %s -decompose-tuples="convert-scf-ops=false" \
+// RUN: | FileCheck --enable-var-scope --check-prefix=CHECK-NO-SCF %s
+
+!nested_tuple = tuple<tuple<>, i32, tuple<i64>>
+
+// CHECK-LABEL: func.func @fromElements(
+// CHECK-SAME:                          %[[ARG0:[^:]*]]: i32,
+// CHECK-SAME:                          %[[ARG1:[^:]*]]: i64) -> (i32, i64) {
+// CHECK-NEXT:    return %[[ARG0]], %[[ARG1]] : i32, i64
+func.func @fromElements(%arg0 : tuple<>, %arg1 : i32, %arg2 : tuple<i64>) -> !nested_tuple {
+  %tuple = tuple.from_elements %arg0, %arg1, %arg2 : !nested_tuple
+  return %tuple : !nested_tuple
+}
+
+// CHECK-LABEL: func.func @toElements(
+// CHECK-SAME:                        %[[ARG0:[^:]*]]: i32,
+// CHECK-SAME:                        %[[ARG1:[^:]*]]: i64) -> (i32, i64) {
+// CHECK-NEXT:    return %[[ARG0]], %[[ARG1]] : i32, i64
+func.func @toElements(%input : !nested_tuple) -> (tuple<>, i32, tuple<i64>) {
+  %elements:3 = tuple.to_elements %input : !nested_tuple
+  return %elements#0, %elements#1, %elements#2 : tuple<>, i32, tuple<i64>
+}
+
+// CHECK-LABEL: func.func @argumentMaterialization(
+// CHECK-SAME:                                     %[[ARG0:[^:]*]]: i32,
+// CHECK-SAME:                                     %[[ARG1:[^:]*]]: i64) -> i32 {
+// CHECK-DAG:     %[[V0:.*]] = tuple.from_elements  : tuple<>
+// CHECK-DAG:     %[[V1:.*]] = tuple.from_elements %[[ARG1]] : tuple<i64>
+// CHECK-NEXT:    %[[V2:.*]] = tuple.from_elements %[[V0]], %[[ARG0]], %[[V1]] : tuple<tuple<>, i32, tuple<i64>>
+// CHECK-NEXT:    %[[V3:.*]] = builtin.unrealized_conversion_cast %[[V2]] : tuple<tuple<>, i32, tuple<i64>> to i32
+func.func @argumentMaterialization(%input : !nested_tuple) -> i32 {
+  %0 = builtin.unrealized_conversion_cast %input : !nested_tuple to i32
+  return %0 : i32
+}
+
+// CHECK-LABEL: func.func @sourceMaterialization() -> i32 {
+// CHECK-DAG:     %[[V0:.*]] = arith.constant 1 : i32
+// CHECK-DAG:     %[[V1:.*]] = arith.constant 2 : i64
+// CHECK-DAG:     %[[V2:.*]] = tuple.from_elements  : tuple<>
+// CHECK-DAG:     %[[V3:.*]] = tuple.from_elements %[[V1]] : tuple<i64>
+// CHECK-NEXT:    %[[V4:.*]] = tuple.from_elements %[[V2]], %[[V0]], %[[V3]] : tuple<tuple<>, i32, tuple<i64>>
+// CHECK-NEXT:    %[[V5:.*]] = builtin.unrealized_conversion_cast %[[V4]] : tuple<tuple<>, i32, tuple<i64>> to i32
+func.func @sourceMaterialization() -> i32 {
+  %i32 = arith.constant 1 : i32
+  %i64 = arith.constant 2 : i64
+  %empty_tuple = tuple.from_elements : tuple<>
+  %i64_tuple = tuple.from_elements %i64 : tuple<i64>
+  %tuple = tuple.from_elements %empty_tuple, %i32, %i64_tuple : !nested_tuple
+  %0 = builtin.unrealized_conversion_cast %tuple : !nested_tuple to i32
+  return %0 : i32
+}
+
+// CHECK-LABEL: func.func @targetMaterialization() -> (i32, i64) {
+// CHECK-DAG:     %[[V0:.*]] = builtin.unrealized_conversion_cast to tuple<tuple<>, i32, tuple<i64>>
+// CHECK-DAG:     %[[V1:.*]]:3 = tuple.to_elements %[[V0]] : tuple<tuple<>, i32, tuple<i64>>
+// CHECK-DAG:     tuple.to_elements %[[V1]]#0 : tuple<>
+// CHECK-DAG:     %[[V2:.*]] = tuple.to_elements %elements#2 : tuple<i64>
+// CHECK-NEXT:    return %[[V1]]#1, %[[V2]] : i32, i64
+func.func @targetMaterialization() -> !nested_tuple {
+  %tuple = builtin.unrealized_conversion_cast to !nested_tuple
+  return %tuple : !nested_tuple
+}
+
+// CHECK-LABEL: func.func @scfIf(
+// CHECK-SAME:                   %[[ARG0:[^:]*]]: i1) -> (i32, i64) {
+// CHECK:         %[[V0:.*]]:2 = scf.if %[[ARG0]] -> (i32, i64) {
+// CHECK-NEXT:      scf.yield %[[V1:.*]], %[[V2:.*]] : i32, i64
+// CHECK-NEXT:    } else {
+// CHECK-NEXT:      scf.yield %[[V3:.*]], %[[V4:.*]] : i32, i64
+// CHECK-NEXT:    }
+// CHECK-NO-SCF-LABEL: func.func @scfIf(
+// CHECK-NO-SCF-SAME:                   %[[ARG0:[^:]*]]: i1) -> (i32, i64) {
+// CHECK-NO-SCF:         %[[V0:.*]] = scf.if %[[ARG0]] -> (tuple<tuple<>, i32, tuple<i64>>) {
+// CHECK-NO-SCF:         %[[V1:.*]]:3 = tuple.to_elements %[[V0]] : tuple<tuple<>, i32, tuple<i64>>
+func.func @scfIf(%cmp : i1) -> !nested_tuple {
+  %result = scf.if %cmp -> !nested_tuple {
+    %i32 = arith.constant 1 : i32
+    %i64 = arith.constant 2 : i64
+    %empty_tuple = tuple.from_elements : tuple<>
+    %i64_tuple = tuple.from_elements %i64 : tuple<i64>
+    %tuple = tuple.from_elements %empty_tuple, %i32, %i64_tuple : !nested_tuple
+    scf.yield %tuple : !nested_tuple
+  } else {
+    %i32 = arith.constant -1 : i32
+    %i64 = arith.constant -2 : i64
+    %empty_tuple = tuple.from_elements : tuple<>
+    %i64_tuple = tuple.from_elements %i64 : tuple<i64>
+    %tuple = tuple.from_elements %empty_tuple, %i32, %i64_tuple : !nested_tuple
+    scf.yield %tuple : !nested_tuple
+  }
+  return %result : !nested_tuple
+}
