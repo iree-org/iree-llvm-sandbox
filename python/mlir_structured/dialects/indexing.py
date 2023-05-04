0da7fe6acc33c05813a377ae7d8bf2d81b668661
max <maksim.levental@gmail.com>
2023-05-03 22:58:52 -0500

    [Indexing] Refactor Scalar, Tensor

--- python/mlir_structured/dialects/indexing.py
+++ python/mlir_structured/dialects/indexing.py
@@ -3,23 +3,40 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+import operator
 import re
-from typing import Union, Tuple
+from copy import deepcopy
+from functools import cached_property, lru_cache, partialmethod
+from typing import Union, Tuple, Optional
 
-from . import arith as arith_dialect
-from . import tensor as tensor_dialect
-# noinspection PyUnresolvedReferences
+import numpy as np
+
+from . import arith, tensor
+from ._arith_ops_ext import _is_integer_like_type
 from ._indexing_ops_gen import *
-from .linalg.opdsl.lang.emitter import _BodyBuilder, _is_floating_point_type, _is_integer_type, \
-  _is_index_type, _is_complex_type
-# noinspection PyUnresolvedReferences
-from .._mlir_libs import _mlirStructuredPasses as _cextStructuredPasses
+from ._ods_common import get_op_result_or_value
+from .linalg.opdsl.lang.emitter import _is_floating_point_type
 from .._mlir_libs._structuredDialects.indexing import *
-from ..ir import Value, Type, RankedTensorType, ShapedType, Operation, IndexType
+from ..ir import (
+    Value,
+    Type,
+    RankedTensorType,
+    ShapedType,
+    Operation,
+    OpView,
+    DenseElementsAttr,
+    IntegerAttr,
+    IntegerType,
+    F16Type,
+    F32Type,
+    F64Type,
+    FloatAttr,
+    IndexType,
+)
+
+# from .._mlir_libs import _mlirStructuredPasses as _cextStructuredPasses
 
 res_val_reg = re.compile(r"(%\w+) =")
-
-_body_builder = _BodyBuilder({}, {}, {})
 
 # The classes in this file implement syntactic sugar for creating MLIR corresponding to arithmetic operations
 # on `tensor`s (MLIR Value whose type is `tensor<...>`) and `scalar`s (`f*` floats, `i*` integers, and `index`).
@@ -29,90 +46,199 @@ _body_builder = _BodyBuilder({}, {}, {})
 # The classes also provide some convenience factory methods and properties for accessing metadata (data type, shape, etc.).
 
 
-class Scalar(ScalarValue):
+def infer_mlir_type(py_val: Union[int, float, bool, np.ndarray]) -> Type:
+  if isinstance(py_val, bool):
+    return IntegerType.get_signless(1)
+  elif isinstance(py_val, int):
+    return IntegerType.get_signless(64)
+  elif isinstance(py_val, float):
+    return F64Type.get()
+  elif isinstance(py_val, np.ndarray):
+    return {
+        np.int8: IntegerType.get_signless(8),
+        np.int16: IntegerType.get_signless(16),
+        np.int32: IntegerType.get_signless(32),
+        np.int64: IntegerType.get_signless(64),
+        np.uintp: IndexType.get(),
+        np.float16: F16Type.get(),
+        np.float32: F32Type.get(),
+        np.float64: F64Type.get(),
+    }[py_val.dtype.type]
+  else:
+    raise NotImplementedError(
+        f"Unsupported Python value {py_val=} with type {type(py_val)}")
 
+
+def constant(
+    cst: Union[int, float, bool, np.ndarray],
+    type: Optional[Type] = None,
+    index: Optional[bool] = None,
+) -> arith.ConstantOp:
+  assert isinstance(cst, (int, float, bool, np.ndarray))
+  if index is not None and index:
+    type = IndexType.get()
+  elif type is None:
+    type = infer_mlir_type(cst)
+  assert type is not None
+
+  if isinstance(cst, np.ndarray):
+    type = RankedTensorType.get(cst.shape, type)
+    cst = DenseElementsAttr.get(
+        cst,
+        type=type,
+    )
+
+  cst = arith.ConstantOp(type, cst)
+
+  return cst
+
+
+# TypeError: metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases
+class ArithValueMeta(type(Value)):
+
+  def __call__(cls, *args, **kwargs):
+    if len(args) != 1:
+      raise ValueError("Only one non-kw arg supported.")
+    arg = args[0]
+    if isinstance(arg, (OpView, Operation, Value)):
+      if isinstance(arg, (Operation, OpView)):
+        assert len(arg.results) == 1
+      val = get_op_result_or_value(arg)
+      arg_copy = None
+    elif isinstance(arg, (int, float, bool, np.ndarray)):
+      dtype = kwargs.get("dtype")
+      index = kwargs.get("index")
+      arg_copy = deepcopy(arg)
+      val = constant(arg, dtype, index).result
+    else:
+      raise NotImplementedError(
+          f"{cls.__name__} doesn't support wrapping {arg}.")
+
+    cls_obj = cls.__new__(cls, val)
+    cls.__init__(cls_obj, val, arg_copy)
+    return cls_obj
+
+
+class ArithValue(metaclass=ArithValueMeta):
+
+  def __new__(cls, val):
+    # The call chain after this is the __init__ here *and then* Value.__init__
+    return super().__new__(cls, val)
+
+  def __init__(self,
+               val,
+               arg: Optional[Union[int, float, bool, np.ndarray]] = None):
+    self.__arg = arg
+    super().__init__(val)
+
+  @lru_cache(maxsize=1)
+  def is_constant(self) -> bool:
+    return isinstance(self.owner.opview, arith.ConstantOp)
+
+  @lru_cache(maxsize=1)
   def __str__(self):
     s = res_val_reg.findall(super().__str__())
     assert len(s) == 1
-    return f"Scalar({s[0]}, {self.type})"
+    if self.is_constant():
+      v = str(self.literal_value)
+      v = f"{v[:10]}..." if len(v) > 10 else v
+      return f"{self.__class__.__name__}({s[0]}, {self.type}, {v})"
+    else:
+      return f"{self.__class__.__name__}({s[0]}, {self.type})"
 
-  def __add__(self, other) -> "Scalar":
-    return Scalar(_body_builder._binary_add(self, other))
+  @lru_cache(maxsize=1)
+  def __repr__(self):
+    return str(self)
 
-  def __sub__(self, other) -> "Scalar":
-    return Scalar(_body_builder._binary_sub(self, other))
+  def __binary_op(self, other: "Tensor", op: str) -> "Tensor":
+    assert op in {"add", "sub", "mul"}
+    if self.type != other.type:
+      raise ValueError(f"{self=} {other=} must have the same type.")
 
-  def __mul__(self, other) -> "Scalar":
-    return Scalar(_body_builder._binary_mul(self, other))
+    if self.is_constant() and other.is_constant():
+      lhs, rhs = self.literal_value, other.literal_value
+      op = operator.attrgetter(op)(operator)
+    else:
+      op = op.capitalize()
+      lhs, rhs = self, other
+      if _is_floating_point_type(self.dtype):
+        op = getattr(arith, f"{op}FOp")
+      elif _is_integer_like_type(self.dtype):
+        op = getattr(arith, f"{op}IOp")
+      else:
+        raise NotImplementedError(f"Unsupported '{op}' operands: {lhs}, {rhs}")
+    return self.__class__(op(lhs, rhs))
+
+  __add__ = partialmethod(__binary_op, op="add")
+  __sub__ = partialmethod(__binary_op, op="sub")
+  __mul__ = partialmethod(__binary_op, op="mul")
 
 
-class Tensor(TensorValue):
+class Scalar(ArithValue, ScalarValue):
 
-  def __str__(self):
-    s = res_val_reg.findall(super().__str__())
-    assert len(s) == 1
-    return f"Tensor({s[0]}, {self.type})"
+  @cached_property
+  def dtype(self) -> Type:
+    return self.type
 
-  @property
+  @cached_property
+  def literal_value(self) -> Union[int, float, bool]:
+    if not self.is_constant():
+      raise ValueError("Can't build literal from non-constant Scalar")
+    return self.owner.opview.literal_value
+
+
+class Tensor(ArithValue, TensorValue):
+
+  @cached_property
+  def literal_value(self) -> np.ndarray:
+    if not self.is_constant():
+      raise ValueError("Can't build literal from non-constant Tensor")
+    return np.array(DenseElementsAttr(self.owner.opview.value), copy=False)
+
+  @cached_property
   def _shaped_type(self) -> ShapedType:
     return ShapedType(self.type)
 
-  @property
-  def shape(self) -> list[int]:
-    assert self._shaped_type.has_static_shape, "Only static shapes currently supported."
-    return self._shaped_type.shape
+  @cached_property
+  def shape(self) -> Tuple[int, ...]:
+    if not self._shaped_type.has_static_shape:
+      raise ValueError("Only static shapes currently supported.")
+    return tuple(self._shaped_type.shape)
 
-  @property
-  def element_type(self) -> Type:
+  @cached_property
+  def dtype(self) -> Type:
     return self._shaped_type.element_type
 
   @classmethod
-  def empty(cls, shape: Union[list[Union[int, Value]], tuple[Union[int, Value],
-                                                             ...]],
-            el_type: Type) -> "Tensor":
+  def empty(
+      cls,
+      shape: Union[list[Union[int, Value]], tuple[Union[int, Value], ...]],
+      el_type: Type,
+  ) -> "Tensor":
 
-    return cls(tensor_dialect.EmptyOp(shape, el_type).result)
+    return cls(tensor.EmptyOp(shape, el_type).result)
 
   def __class_getitem__(
-      cls, dim_sizes_el_type: Tuple[Union[list[int], tuple[int, ...]],
-                                    Type]) -> Type:
-    assert (len(dim_sizes_el_type) == 2
-           ), f"wrong dim_sizes_el_type: {dim_sizes_el_type}"
-    dim_sizes, el_type = dim_sizes_el_type
-    assert isinstance(el_type, Type), f"wrong type T args for tensor: {el_type}"
+      cls, dim_sizes_dtype: Tuple[Union[list[int], tuple[int, ...]],
+                                  Type]) -> Type:
+    if len(dim_sizes_dtype) != 2:
+      raise ValueError(
+          f"Wrong type of argument to {cls.__name__}: {dim_sizes_dtype=}")
+    dim_sizes, dtype = dim_sizes_dtype
+    if not isinstance(dtype, Type):
+      raise ValueError(f"{dtype=} is not {Type=}")
     static_sizes = []
     for s in dim_sizes:
       if isinstance(s, int):
         static_sizes.append(s)
       else:
         static_sizes.append(ShapedType.get_dynamic_size())
-    return RankedTensorType.get(static_sizes, el_type)
+    return RankedTensorType.get(static_sizes, dtype)
 
   def __getitem__(self, dims: tuple) -> Scalar:
     dims = list(dims)
     for i, d in enumerate(dims):
       if isinstance(d, int):
-        dims[i] = arith_dialect.ConstantOp.create_index(d).result
+        dims[i] = arith.ConstantOp.create_index(d).result
 
-    return Scalar(tensor_dialect.ExtractOp(self, dims).result)
-
-  @classmethod
-  def __binary_op(cls, op: str, lhs: "Tensor", rhs: "Tensor") -> "Tensor":
-    assert op in {"Add", "Sub", "Mul"}
-    assert lhs.element_type == rhs.element_type
-    assert lhs.shape == rhs.shape
-
-    if _is_floating_point_type(lhs.element_type):
-      return cls(getattr(arith_dialect, f"{op}FOp")(lhs, rhs).result)
-    if _is_integer_type(lhs.element_type) or _is_index_type(lhs.element_type):
-      return cls(getattr(arith_dialect, f"{op}IOp")(lhs, rhs).result)
-    raise NotImplementedError(f"Unsupported '{op}' operands: {lhs}, {rhs}")
-
-  def __add__(self: "Tensor", rhs: "Tensor") -> "Tensor":
-    return self.__binary_op("Add", self, rhs)
-
-  def __sub__(self: "Tensor", rhs: "Tensor") -> "Tensor":
-    return self.__binary_op("Sub", self, rhs)
-
-  def __mul__(self: "Tensor", rhs: "Tensor") -> "Tensor":
-    return self.__binary_op("Mul", self, rhs)
+    return Scalar(tensor.ExtractOp(self, dims))
--- test/python/dialects/indexing/dialect.py
+++ test/python/dialects/indexing/dialect.py
@@ -1,8 +1,11 @@
 # RUN: %PYTHON %s | FileCheck %s
+from random import random
 
-from mlir_structured.dialects import indexing as idx
-from mlir_structured.dialects import func
-from mlir_structured.ir import Context, IntegerType, F32Type
+import numpy as np
+
+from mlir_structured.dialects import func, arith, indexing
+from mlir_structured.dialects.indexing import Scalar, Tensor, IndexTensorType
+from mlir_structured.ir import Context, IntegerType, F64Type
 from mlir_structured.passmanager import PassManager
 from mlir_structured.runtime.util import mlir_mod_ctx
 
@@ -10,34 +13,80 @@ from mlir_structured.runtime.util import mlir_mod_ctx
 def run(f):
   print("\nTEST:", f.__name__)
   with Context():
-    idx.register_dialect()
+    indexing.register_dialect()
     f()
   return f
 
 
-# CHECK-LABEL: TEST: testArithValue
+# CHECK-LABEL: TEST: testScalarValue
 @run
-def testArithValue():
+def testScalarValue():
+  f64 = F64Type.get()
   i32 = IntegerType.get_signless(32)
-  with mlir_mod_ctx():
-    ten = idx.Tensor.empty([10, 10], i32)
-    # CHECK: %[[TEN:.*]] = tensor.empty() : tensor<10x10xi32>
-    print(ten.owner)
-    # CHECK: Tensor(%[[TEN]], tensor<10x10xi32>)
-    print(ten)
+  with mlir_mod_ctx() as module:
+    zero_f64 = Scalar(arith.ConstantOp(f64, 0.0).result)
+    # CHECK: Scalar(%{{.*}}, f64, 0.0)
+    print(zero_f64)
+    # CHECK: True
+    print(zero_f64.is_constant())
+    # CHECK: 0.0
+    print(zero_f64.literal_value)
 
-    v = ten[0, 0]
-    # CHECK: %[[EXTRACTED:.*]] = tensor.extract %[[TEN]][%{{.*}}, %{{.*}}] : tensor<10x10xi32>
-    print(v.owner)
-    # CHECK: Scalar(%[[EXTRACTED]], i32)
-    print(v)
+    zero_f64 = Scalar(arith.ConstantOp(f64, 0.0))
+    # CHECK: Scalar(%{{.*}}, f64, 0.0)
+    print(zero_f64)
+    # CHECK: True
+    print(zero_f64.is_constant())
+    # CHECK: 0.0
+    print(zero_f64.literal_value)
 
-    w = v + v
-    # CHECK: %[[ADDI:.*]] = arith.addi %[[EXTRACTED]], %[[EXTRACTED]] : i32
-    print(w.owner)
-    z = w * w
-    # CHECK: %[[MULI:.*]] = arith.muli %[[ADDI]], %[[ADDI]] : i32
-    print(z.owner)
+    zero_f64 = Scalar(0.0)
+    # CHECK: Scalar(%{{.*}}, f64, 0.0)
+    print(zero_f64)
+    # CHECK: True
+    print(zero_f64.is_constant())
+    # CHECK: 0.0
+    print(zero_f64.literal_value)
+
+    zero_i64 = Scalar(0)
+    # CHECK: Scalar(%{{.*}}, i64, 0)
+    print(zero_i64)
+    # CHECK: True
+    print(zero_i64.is_constant())
+    # CHECK: 0
+    print(zero_i64.literal_value)
+
+    zero_i32 = Scalar(0, dtype=i32)
+    # CHECK: Scalar(%{{.*}}, i32, 0)
+    print(zero_i32)
+    # CHECK: True
+    print(zero_i32.is_constant())
+    # CHECK: 0
+    print(zero_i32.literal_value)
+
+    zero_index = Scalar(0, index=True)
+    # CHECK: Scalar(%{{.*}}, index, 0)
+    print(zero_index)
+    # CHECK: True
+    print(zero_index.is_constant())
+    # CHECK: 0
+    print(zero_index.literal_value)
+
+    one_f64 = Scalar(1.0)
+    two_f64 = Scalar(2.0)
+
+    three_f64 = one_f64 + two_f64
+    # CHECK: %{{.*}} = arith.constant 3.000000e+00 : f64
+    print(three_f64.owner)
+
+    x, y = random(), random()
+    x_f64, y_f64 = Scalar(x), Scalar(y)
+
+    z_f64 = x_f64 + y_f64
+    # CHECK: True
+    print(z_f64.literal_value == x + y)
+    # CHECK: True
+    print(zero_f64.is_constant())
 
 
 # CHECK-LABEL: TEST: testTensorType
@@ -45,15 +94,15 @@ def testArithValue():
 def testTensorType():
   i32 = IntegerType.get_signless(32)
   with mlir_mod_ctx():
-    tt = idx.Tensor[(10, 10), i32]
+    tt = Tensor[(10, 10), i32]
     # CHECK: tensor<10x10xi32>
     print(tt)
 
-    tt = idx.Tensor[(None, None), i32]
+    tt = Tensor[(None, None), i32]
     # CHECK: tensor<?x?xi32>
     print(tt)
 
-    tt = idx.IndexTensorType.get([10, 10])
+    tt = IndexTensorType.get([10, 10])
     # CHECK: tensor<10x10xindex>
     print(tt)
 
@@ -64,72 +113,115 @@ def testTensorValue():
   i32 = IntegerType.get_signless(32)
   with mlir_mod_ctx() as module:
 
-    @func.FuncOp.from_py_func()
-    def test_tensor_value():
-      ten = idx.Tensor.empty((10, 10), i32)
-      # CHECK: Tensor(%[[TEN:.*]], tensor<10x10xi32>)
-      print(ten)
+    ten = Tensor.empty((10, 10), i32)
+    # CHECK: Tensor(%[[TEN:.*]], tensor<10x10xi32>)
+    print(repr(ten))
+    # CHECK: %[[TEN]] = tensor.empty() : tensor<10x10xi32>
+    print(ten.owner)
+    # CHECK: (10, 10)
+    print(ten.shape)
+    # CHECK: i32
+    print(ten.dtype)
+    # CHECK: False
+    print(ten.is_constant())
+    try:
+      print(ten.literal_value)
+    except ValueError as e:
+      # CHECK: Can't build literal from non-constant Tensor
+      print(e)
 
-      sum_ten = ten + ten
-      # CHECK: %[[ADD:.*]] = "arith.addi"(%[[TEN]], %[[TEN]]) : (tensor<10x10xi32>, tensor<10x10xi32>) -> tensor<10x10xi32>
-      print(sum_ten.owner)
+    sum_ten_1 = ten + ten
+    # CHECK: %[[ADD:.*]] = arith.addi %[[TEN]], %[[TEN]] : tensor<10x10xi32>
+    print(sum_ten_1.owner)
 
-      prod_ten = ten * ten
-      # CHECK: %[[MUL:.*]] = "arith.muli"(%[[TEN]], %[[TEN]]) : (tensor<10x10xi32>, tensor<10x10xi32>) -> tensor<10x10xi32>
-      print(prod_ten.owner)
+    prod_ten = ten * ten
+    # CHECK: %[[MUL:.*]] = arith.muli %[[TEN]], %[[TEN]] : tensor<10x10xi32>
+    print(prod_ten.owner)
 
-      return prod_ten
+    x = np.random.random((10, 10))
+    ten_x = Tensor(x)
+    # CHECK: Tensor(%[[CST1:.*]], tensor<10x10xf64>, [
+    print(ten_x)
+    # CHECK: (10, 10)
+    print(ten_x.shape)
+    # CHECK: f64
+    print(ten_x.dtype)
+    # CHECK: True
+    print(ten_x.is_constant())
+    # CHECK: True
+    print(np.allclose(ten_x.literal_value, x))
+
+    y = np.random.random((10, 10))
+    # CHECK: Tensor(%[[CST2:.*]], tensor<10x10xf64>, [
+    ten_y = Tensor(y)
+    print(ten_y)
+    sum_ten_2 = ten_x + ten_y
+    # CHECK: Tensor(%[[CST3:.*]], tensor<10x10xf64>, [
+    print(sum_ten_2)
+    # CHECK: (10, 10)
+    print(sum_ten_2.shape)
+    # CHECK: f64
+    print(sum_ten_2.dtype)
+    # CHECK: True
+    print(sum_ten_2.is_constant())
+    # CHECK: True
+    print(np.allclose(sum_ten_2.literal_value, x + y))
+
+    try:
+      Tensor(arith.ConstantOp(i32, 0).result)
+    except ValueError as e:
+      # CHECK: Cannot cast value to TensorValue (from <mlir_structured._mlir_libs._mlir.ir.OpResult
+      print(e)
 
   # CHECK: module {
-  # CHECK:   func.func @test_tensor_value() -> tensor<10x10xi32> {
-  # CHECK:     %[[TEN]] = tensor.empty() : tensor<10x10xi32>
-  # CHECK:     %[[ADD]] = arith.addi %[[TEN]], %[[TEN]] : tensor<10x10xi32>
-  # CHECK:     %[[MUL]] = arith.muli %[[TEN]], %[[TEN]] : tensor<10x10xi32>
-  # CHECK:     return %[[MUL]] : tensor<10x10xi32>
-  # CHECK:   }
+  # CHECK:   %[[TEN]] = tensor.empty() : tensor<10x10xi32>
+  # CHECK:   %[[ADD]] = arith.addi %[[TEN]], %[[TEN]] : tensor<10x10xi32>
+  # CHECK:   %[[MUL]] = arith.muli %[[TEN]], %[[TEN]] : tensor<10x10xi32>
+  # CHECK:   %[[CST1]] = arith.constant dense<{{.*}}> : tensor<10x10xf64>
+  # CHECK:   %[[CST2]] = arith.constant dense<{{.*}}> : tensor<10x10xf64>
+  # CHECK:   %[[CST3]] = arith.constant dense<{{.*}}> : tensor<10x10xf64>
   # CHECK: }
   print(module)
 
-  pm = PassManager.parse(
-      'builtin.module(func.func(convert-elementwise-to-linalg))')
+  pm = PassManager.parse('builtin.module(convert-elementwise-to-linalg)')
   pm.run(module.operation)
 
   # CHECK: #map = affine_map<(d0, d1) -> (d0, d1)>
   # CHECK: module {
-  # CHECK:   func.func @test_tensor_value() -> tensor<10x10xi32> {
-  # CHECK:     %{{.*}} = tensor.empty()
-  # CHECK:     %{{.*}} = linalg.generic
-  # CHECK:     ^bb0(%{{.*}}: i32, %{{.*}}: i32, %{{.*}}: i32):
-  # CHECK:       %{{.*}} = arith.addi %{{.*}}, %{{.*}} : i32
-  # CHECK:       linalg.yield %{{.*}} : i32
-  # CHECK:     } -> tensor<10x10xi32>
-  # CHECK:     %{{.*}} = linalg.generic
-  # CHECK:     ^bb0(%{{.*}}: i32, %{{.*}}: i32, %{{.*}}: i32):
-  # CHECK:       %{{.*}} = arith.muli %{{.*}}, %{{.*}} : i32
-  # CHECK:       linalg.yield %{{.*}} : i32
-  # CHECK:     } -> tensor<10x10xi32>
-  # CHECK:     return %{{.*}} : tensor<10x10xi32>
-  # CHECK:   }
+  # CHECK:   %{{.*}} = tensor.empty()
+  # CHECK:   %{{.*}} = linalg.generic
+  # CHECK:   ^bb0(%{{.*}}: i32, %{{.*}}: i32, %{{.*}}: i32):
+  # CHECK:     %{{.*}} = arith.addi %{{.*}}, %{{.*}} : i32
+  # CHECK:     linalg.yield %{{.*}} : i32
+  # CHECK:   } -> tensor<10x10xi32>
+  # CHECK:   %{{.*}} = linalg.generic
+  # CHECK:   ^bb0(%{{.*}}: i32, %{{.*}}: i32, %{{.*}}: i32):
+  # CHECK:     %{{.*}} = arith.muli %{{.*}}, %{{.*}} : i32
+  # CHECK:     linalg.yield %{{.*}} : i32
+  # CHECK:   } -> tensor<10x10xi32>
+  # CHECK:   %[[CST1]] = arith.constant dense<{{.*}}> : tensor<10x10xf64>
+  # CHECK:   %[[CST2]] = arith.constant dense<{{.*}}> : tensor<10x10xf64>
+  # CHECK:   %[[CST3]] = arith.constant dense<{{.*}}> : tensor<10x10xf64>
   # CHECK: }
   print(module)
 
 
-# CHECK-LABEL: TEST: testConcatOp
+# CHECK-LABEL: TEST: testConcatenateOp
 @run
-def testConcatOp():
+def testConcatenateOp():
   i32 = IntegerType.get_signless(32)
   with mlir_mod_ctx() as module:
 
     @func.FuncOp.from_py_func()
     def test_concat_op():
-      ten = idx.Tensor.empty((10, 10), i32)
+      ten = Tensor.empty((10, 10), i32)
       # CHECK: Tensor(%[[TEN:.*]], tensor<10x10xi32>)
       print(ten)
 
-      concat_ten_first_dim = idx.ConcatenateOp((ten, ten), 0).result
+      concat_ten_first_dim = indexing.ConcatenateOp((ten, ten), 0).result
       # CHECK: %{{.*}} = "indexing.concatenate"(%[[TEN]], %[[TEN]]) {dimension = 0 : i64} : (tensor<10x10xi32>, tensor<10x10xi32>) -> tensor<20x10xi32>
       print(concat_ten_first_dim.owner)
 
-      concat_ten_second_dim = idx.ConcatenateOp((ten, ten), 1).result
+      concat_ten_second_dim = indexing.ConcatenateOp((ten, ten), 1).result
       # CHECK: %{{.*}} = "indexing.concatenate"(%[[TEN]], %[[TEN]]) {dimension = 1 : i64} : (tensor<10x10xi32>, tensor<10x10xi32>) -> tensor<10x20xi32>
       print(concat_ten_second_dim.owner)
