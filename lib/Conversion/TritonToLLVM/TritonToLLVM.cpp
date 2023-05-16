d859d8c06e6fb5a026605288535b7ea1f2b8c9de
Ingo Müller <ingomueller@google.com>
2023-05-16 12:24:11 +0000

    Add new TritonToLLVM pass that lowers tt.func etc to LLVM.

--- lib/Conversion/TritonToLLVM/TritonToLLVM.cpp
+++ lib/Conversion/TritonToLLVM/TritonToLLVM.cpp
@@ -9,30 +9,72 @@
 #include "structured/Conversion/TritonToLLVM/TritonToLLVM.h"
 
 #include "../PassDetail.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/Transforms/DialectConversion.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
 
 namespace mlir {
 class MLIRContext;
 } // namespace mlir
 
 using namespace mlir;
+using namespace mlir::func;
 using namespace mlir::LLVM;
+using namespace triton;
 
 namespace {
 struct ConvertTritonToLLVMPass
     : public ConvertTritonToLLVMBase<ConvertTritonToLLVMPass> {
   void runOnOperation() override;
 };
+
+/// Replaces an op of type SourceOp to an op of type TargetOp while preserving
+/// all types, operands, attributes, successors, regions, and its location.
+template <typename SourceOp, typename TargetOp>
+struct OneToOneOpConversion : public OpConversionPattern<SourceOp> {
+  OneToOneOpConversion(TypeConverter &typeConverter, MLIRContext *context,
+                       PatternBenefit benefit = 1)
+      : OpConversionPattern<SourceOp>(typeConverter, context, benefit) {}
+
+  using OpAdaptor = typename OpConversionPattern<SourceOp>::OpAdaptor;
+
+  LogicalResult
+  matchAndRewrite(SourceOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    MLIRContext *context = this->getContext();
+    SmallVector<std::unique_ptr<Region>> regions;
+    for (auto &region : op->getRegions()) {
+      auto &newRegion = regions.emplace_back(new Region);
+      rewriter.inlineRegionBefore(region, *newRegion, newRegion->end());
+    }
+    Operation *newOp = rewriter.create(
+        op->getLoc(), StringAttr::get(context, TargetOp::getOperationName()),
+        op->getOperands(), op->getResultTypes(), op->getAttrs(),
+        op->getSuccessors(), regions);
+    rewriter.replaceOp(op, newOp->getResults());
+    return success();
+  }
+};
 } // namespace
 
 void mlir::populateTritonToLLVMConversionPatterns(
-    RewritePatternSet &patterns, TypeConverter &typeConverter) {}
+    RewritePatternSet &patterns, TypeConverter &typeConverter) {
+  patterns.add<
+      // clang-format off
+      OneToOneOpConversion<triton::CallOp, func::CallOp>,
+      OneToOneOpConversion<triton::FuncOp, func::FuncOp>,
+      OneToOneOpConversion<triton::ReturnOp, func::ReturnOp>
+      // clang-format on
+      >(typeConverter, patterns.getContext());
+}
 
 void ConvertTritonToLLVMPass::runOnOperation() {
   auto module = getOperation();
-  TypeConverter typeConverter;
+  LLVMTypeConverter typeConverter(&getContext());
 
   // Convert the remaining ops of this dialect using dialect conversion.
   ConversionTarget target(getContext());
@@ -40,7 +82,12 @@ void ConvertTritonToLLVMPass::runOnOperation() {
   target.addLegalOp<ModuleOp>();
   RewritePatternSet patterns(&getContext());
 
+  // Lower tt.func op and friends to corresponding ops from func.
   populateTritonToLLVMConversionPatterns(patterns, typeConverter);
+
+  // Lower ops from func to LLVM.
+  populateFuncToLLVMFuncOpConversionPattern(typeConverter, patterns);
+  populateFuncToLLVMConversionPatterns(typeConverter, patterns);
 
   // Use UnrealizedConversionCast as materializations, which have to be cleaned
   // up by later passes.
