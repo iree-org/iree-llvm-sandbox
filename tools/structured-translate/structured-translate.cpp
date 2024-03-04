b3d2cc847fd35611328f4f924d174c6e2678d6be
Ingo Müller <ingomueller@google.com>
2024-03-04 13:06:16 +0000

    Extend file documentation of structured-translate.cpp.

--- tools/structured-translate/structured-translate.cpp
+++ tools/structured-translate/structured-translate.cpp
@@ -1,9 +1,12 @@
-//===-- structured-translate.cpp - custom mlir-translate --------*- C++ -*-===//
+//===-- structured-translate.cpp - "structured" mlir-translate --*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+// `mlir-stranslate` with translations to and from "structured" dialects, i.e.,
+// dialects from this repository.
 //===----------------------------------------------------------------------===//
 
 #include "mlir/IR/BuiltinOps.h"
