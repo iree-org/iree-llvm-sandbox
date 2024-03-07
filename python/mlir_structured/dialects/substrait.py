09ff57ad333316f787561318c26246b070e67416
Ingo Müller <ingomueller@google.com>
2024-03-07 14:02:19 +0000

    [Substrait] Improve Python bindings of PlanOp and PlanRelOp.

--- python/mlir_structured/dialects/substrait.py
+++ python/mlir_structured/dialects/substrait.py
@@ -5,4 +5,36 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ._substrait_ops_gen import *
+from ._substrait_ops_gen import _Dialect
 from .._mlir_libs._structuredDialects.substrait import *
+
+try:
+  from .. import ir
+  from ._ods_common import (
+      _cext as _ods_cext,)
+except ImportError as e:
+  raise RuntimeError("Error loading imports from extension module") from e
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class PlanOp(PlanOp):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.regions[0].blocks.append()
+
+  @property
+  def body(self) -> ir.Block:
+    return self.regions[0].blocks[0]
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class PlanRelOp(PlanRelOp):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.regions[0].blocks.append()
+
+  @property
+  def body(self) -> ir.Block:
+    return self.regions[0].blocks[0]
