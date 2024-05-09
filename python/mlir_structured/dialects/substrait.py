596bc9bfee5606a046711f4e91ea2f5d1996c5ff
Ingo Müller <ingomueller@google.com>
2024-05-09 16:18:17 +0200

    [Substrait] Improve Python bindings of PlanOp and PlanRelOp. (#804)

--- python/mlir_structured/dialects/substrait.py
+++ python/mlir_structured/dialects/substrait.py
@@ -4,5 +4,47 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from typing import Optional, Sequence
+
 from ._substrait_ops_gen import *
+from ._substrait_ops_gen import _Dialect
 from .._mlir_libs._structuredDialects.substrait import *
+
+try:
+  from .. import ir
+  from ._ods_common import (
+      _cext as _ods_cext,)
+except ImportError as e:
+  raise RuntimeError("Error loading imports from extension module") from e
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class PlanOp(PlanOp):
+
+  def __init__(self, *args, version: Optional[Sequence[int]] = None, **kwargs):
+    if version is not None:
+      major, minor, patch = version
+      for part in ["major", "minor", "patch"]:
+        if (part + "_number") in kwargs:
+          raise ValueError(
+              "'version' and '(major|minor|patch)_number' are mutually exclusive"
+          )
+      args = (major, minor, patch) + args
+    super().__init__(*args, **kwargs)
+    self.regions[0].blocks.append()
+
+  @property
+  def body(self) -> ir.Block:
+    return self.regions[0].blocks[0]
+
+
+@_ods_cext.register_operation(_Dialect, replace=True)
+class PlanRelOp(PlanRelOp):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.regions[0].blocks.append()
+
+  @property
+  def body(self) -> ir.Block:
+    return self.regions[0].blocks[0]
--- test/python/dialects/substrait/dialect.py
+++ test/python/dialects/substrait/dialect.py
@@ -1,12 +1,12 @@
 # RUN: %PYTHON %s | FileCheck %s
 
 from mlir_structured.dialects import substrait as ss
-from mlir_structured.ir import Context, Location
+from mlir_structured import ir
 
 
 def run(f):
   print("\nTEST:", f.__name__)
-  with Context(), Location.unknown():
+  with ir.Context(), ir.Location.unknown():
     ss.register_dialect()
     f()
   return f
@@ -15,6 +15,37 @@ def run(f):
 # CHECK-LABEL: TEST: testSubstraitDialect
 @run
 def testSubstraitDialect():
-  plan = ss.PlanOp(0, 42, 1)
+  plan = ss.PlanOp(version=(0, 42, 1))
   print(plan)
   # CHECK: substrait.plan
+
+
+# CHECK-LABEL: TEST: testPlanOp
+@run
+def testPlanOp():
+  plan = ss.PlanOp(0, 42, 1)
+  print(plan)
+  # CHECK: substrait.plan version 0 : 42 : 1
+  plan = ss.PlanOp(version=(0, 42, 1))
+  print(plan)
+  # CHECK: substrait.plan version 0 : 42 : 1
+
+
+# CHECK-LABEL: TEST: testNamedTable
+@run
+def testNamedTable():
+  plan = ss.PlanOp(version=(0, 42, 1))
+
+  with ir.InsertionPoint(plan.body):
+    plan_rel = ss.PlanRelOp()
+    with ir.InsertionPoint(plan_rel.body):
+      si32 = ir.IntegerType.get_signed(32)
+      result_type = ir.TupleType.get_tuple([si32, si32])
+      field_names = ir.ArrayAttr.get([ir.StringAttr.get(n) for n in ["a", "b"]])
+      named_table = ss.NamedTableOp(result_type, "t", field_names)
+      ss.YieldOp(named_table)
+
+  print(plan)
+  # CHECK: substrait.plan
+  # CHECK: relation {
+  # CHECK: named_table @t
